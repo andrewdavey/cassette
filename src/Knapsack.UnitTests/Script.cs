453ee2c8f67ae1c64bea88a016cdd74b765ce96d
Andrew Davey <andrew@equin.co.uk>
2011-04-14 10:12:25 +0100

    Create project. Start coding.

---
+++ src/Knapsack.UnitTests/Script.cs
@@ -0,0 +1,37 @@
+﻿using Should;
+using Xunit;
+
+namespace Knapsack
+{
+    public class Given_a_new_Script
+    {
+        readonly Script script;
+
+        public Given_a_new_Script()
+        {
+            script = new Script(
+                @"c:\scripts\test.js",
+                new byte[] { 1, 2, 3 },
+                new string[] { @"c:\scripts\other.js" }
+            );
+        }
+
+        [Fact]
+        public void has_Path_property()
+        {
+            script.Path.ShouldEqual(@"c:\scripts\test.js");
+        }
+
+        [Fact]
+        public void has_Hash_property()
+        {
+            script.Hash.ShouldEqual(new byte[] { 1,2,3 });
+        }
+
+        [Fact]
+        public void has_References_property()
+        {
+            script.References.ShouldEqual(new[] { @"c:\scripts\other.js" });
+        }
+    }
+}
---
+++ src/Knapsack/Script.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Knapsack
+{
+    public class Script
+    {
+        private string path;
+        private byte[] hash;
+        private string[] references;
+
+        public Script(string path, byte[] hash, string[] references)
+        {
+            this.path = path;
+            this.hash = hash;
+            this.references = references;
+        }
+
+        public string Path
+        {
+            get { return path; }
+        }
+
+        public byte[] Hash
+        {
+            get { return hash; }
+        }
+
+        public string[] References
+        {
+            get { return references; }
+        }
+    }
+}
