1c25b6fd1e373338cac4e781a8065c7414245cf3
Andrew Davey <andrew@equin.co.uk>
2011-04-14 16:37:46 +0100

    Add ModuleWriter using AjaxMin.

---
+++ src/Knapsack.UnitTests/ModuleWriter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Should;
+using Xunit;
+
+namespace Knapsack
+{
+    public class ModuleWriter_facts
+    {
+        [Fact]
+        public void Write_minifies_all_script_content_in_given_order()
+        {
+            using (var textWriter = new StringWriter())
+            {
+                var module = new Module("a", new[] { CreateScript("1"), CreateScript("2") }, new string[0]);
+                var sources = new Dictionary<string, string>
+                {
+                    { "a\\1.js", "function test1 () { }" },
+                    { "a\\2.js", "function test2 () { }" }
+                };
+                var moduleWriter = new ModuleWriter(textWriter, path => sources[path]);
+
+                moduleWriter.Write(module);
+
+                var output = textWriter.GetStringBuilder().ToString();
+                output.ShouldEqual("function test1(){}\r\nfunction test2(){}");
+            }
+        }
+
+        Script CreateScript(string name)
+        {
+            return new Script("a\\" + name + ".js", new byte[0], new string[0]);
+        }
+    }
+}
---
+++ src/Knapsack/ModuleWriter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Microsoft.Ajax.Utilities;
+
+namespace Knapsack
+{
+    public class ModuleWriter
+    {
+        readonly TextWriter textWriter;
+        readonly Func<string, string> loadSourceFromFile;
+
+        public ModuleWriter(TextWriter textWriter, Func<string, string> loadSourceFromFile)
+        {
+            this.textWriter = textWriter;
+            this.loadSourceFromFile = loadSourceFromFile;
+        }
+
+        public void Write(Module module)
+        {
+            var minifier = new Minifier();
+            var first = true;
+            foreach (var script in module.Scripts)
+            {
+                if (!first) textWriter.WriteLine();
+                first = false;
+
+                var source = loadSourceFromFile(script.Path);
+                var minifiedSource = minifier.MinifyJavaScript(source);
+                textWriter.Write(minifiedSource);
+            }
+        }
+    }
+}
