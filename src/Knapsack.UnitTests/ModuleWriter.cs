8ca9b6a26f987a2dbac913f4331082206d532400
Andrew Davey <andrew@equin.co.uk>
2011-05-10 12:18:09 +0100

    Add ModuleWriter test for coffeescript support.

--- src/Knapsack.UnitTests/ModuleWriter.cs
+++ src/Knapsack.UnitTests/ModuleWriter.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using Should;
 using Xunit;
+using Knapsack.CoffeeScript;
 
 namespace Knapsack
 {
@@ -15,8 +16,8 @@ namespace Knapsack
                 var module = new Module("a", new[] { CreateScript("1"), CreateScript("2") }, new string[0]);
                 var sources = new Dictionary<string, string>
                 {
-                    { "a//1.js", "function test1 () { }" },
-                    { "a//2.js", "function test2 () { }" }
+                    { "a/1.js", "function test1 () { }" },
+                    { "a/2.js", "function test2 () { }" }
                 };
                 var moduleWriter = new ModuleWriter(textWriter, "", path => sources[path], null);
 
@@ -27,9 +28,37 @@ namespace Knapsack
             }
         }
 
+        [Fact]
+        public void Write_compiles_coffee_script()
+        {
+            using (var textWriter = new StringWriter())
+            {
+                var module = new Module("a", new[] { new Script("a/test.coffee", new byte[0], new string[0]) }, new string[0]);
+                var sources = new Dictionary<string, string>
+                {
+                    { "a/test.coffee", "x = 1" }
+                };
+                var moduleWriter = new ModuleWriter(textWriter, "", path => sources[path], new FakeCompiler());
+
+                moduleWriter.Write(module);
+
+                var output = textWriter.GetStringBuilder().ToString();
+                output.ShouldEqual("compiled");
+            }
+        }
+
         Script CreateScript(string name)
         {
-            return new Script("a//" + name + ".js", new byte[0], new string[0]);
+            return new Script("a/" + name + ".js", new byte[0], new string[0]);
         }
+
+        class FakeCompiler : ICoffeeScriptCompiler
+        {
+            public string CompileFile(string path)
+            {
+                return "compiled";
+            }
+        }
+
     }
 }
