411a8d82eb6264f6d7817ff9c3ee7038d49de0c4
Andrew Davey <andrew@equin.co.uk>
2011-05-15 15:36:30 +0100

    Change css reference syntax to use special comments instead of @import.

--- src/Knapsack/UnresolvedCssParser.cs
+++ src/Knapsack/UnresolvedCssParser.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 using Knapsack.Utilities;
 
@@ -18,29 +17,39 @@ namespace Knapsack
             );
         }
 
-        readonly Regex[] importRegexes = new Regex[]
-        {
-            new Regex(
-                @"@import \s+ (?<quote>[""']) (?<url>.*?) \<quote> \s* ;",
-                RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace
-            ),
-            new Regex(
-                @"@import \s+ url \s* \( \s* (?<quote>[""']?) (?<url>.*?) \<quote> \s* \) \s* ;",
-                RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace
-            )
-        };
+        readonly Regex cssCommentRegex = new Regex(
+            @"/\*(?<body>.*?)\*/",
+            RegexOptions.Singleline
+        );
+
+        readonly Regex referenceRegex = new Regex(
+            @"@reference \s+ (?<quote>[""']) (?<path>.*?) \<quote> \s* ;",
+            RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace
+        );
 
         IEnumerable<string> ParseReferences(Stream source)
         {
+            // Knapsack looks inside CSS comments for special "reference" declarations.
+            // e.g. /* @reference "something.css"; */
+            // Using @import instead was considered, but may interfere with intended behavior.
+            // Also, it would require removing the @import directives from the outgoing CSS.
+            // Using a comment based declaration is easier to implement and works much like
+            // the JavaScript referencing system.
+
             source.Position = 0;
             using (var reader = new StreamReader(source))
             {
                 var css = reader.ReadToEnd();
 
-                return importRegexes
-                    .SelectMany(regex => regex.Matches(css).Cast<Match>())
-                    .Where(match => match.Groups["url"].Success)
-                    .Select(match => match.Groups["url"].Value);
+                var commentBodies = cssCommentRegex
+                    .Matches(css)
+                    .Cast<Match>()
+                    .Select(match => match.Groups["body"].Value);
+
+                return from body in commentBodies
+                       from match in referenceRegex.Matches(body).Cast<Match>()
+                       where match.Groups["path"].Success
+                       select match.Groups["path"].Value;
             }
         }
     }
