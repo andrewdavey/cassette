552e14be9b360068d4682d32f66ba93721ac9a8e
Andrew Davey <andrew@equin.co.uk>
2011-07-12 16:42:31 +0100

    Use Jurassic nuget package instead of own lib copy. Bump to version v0.4.3.

--- build.bat
+++ build.bat
@@ -8,4 +8,4 @@ nuget pack -Symbols src\Knapsack\Knapsack.csproj -OutputDirectory build\nuget
 
 mkdir build\shell
 msbuild /p:Configuration=Release src\Knapsack.Shell\Knapsack.Shell.csproj
-tools\ilrepack.exe /out:build\shell\knapsack.exe src\Knapsack.Shell\bin\Release\Knapsack.Shell.exe src\Knapsack\bin\Release\Knapsack.dll lib\Jurassic.dll src\Knapsack\bin\Release\AjaxMin.dll
+tools\ilrepack.exe /out:build\shell\knapsack.exe src\Knapsack.Shell\bin\Release\Knapsack.Shell.exe src\Knapsack\bin\Release\Knapsack.dll src\Knapsack\bin\Release\AjaxMin.dll src\Knapsack\bin\Release\Jurassic.dll
