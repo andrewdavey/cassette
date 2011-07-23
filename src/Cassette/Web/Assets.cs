f850d0b68438b1174c53fa36cc57d8fc45facc95
Andrew Davey <andrew@equin.co.uk>
2011-07-23 12:55:49 +0100

    Add Assets static class as page API entry point.

---
+++ src/Cassette/Web/Assets.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Cassette.Web
+{
+    /// <summary>
+    /// Global entry point for all view page helpers.
+    /// </summary>
+    public static class Assets
+    {
+        public static object Scripts
+        {
+            get { return null; }
+        }
+
+        public static object Stylesheets
+        {
+            get { return null; }
+        }
+
+        public static object HtmlTemplates
+        {
+            get { return null; }
+        }
+    }
+}
