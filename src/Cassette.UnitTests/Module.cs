ae3396e0e038bbcb9c561d1b569f9cee69057cd2
Andrew Davey <andrew@equin.co.uk>
2011-08-06 07:21:34 +0100

    Add basic module tests and ContainsPath method.

--- src/Cassette.UnitTests/Module.cs
+++ src/Cassette.UnitTests/Module.cs
@@ -8,6 +8,74 @@ namespace Cassette
     public class Module_Tests
     {
         [Fact]
+        public void ConstructorNormalizesDirectoryPathByRemovingTrailingBackSlash()
+        {
+            var module = new Module("c:\\test\\");
+            module.Directory.ShouldEqual("c:\\test");
+        }
+
+        [Fact]
+        public void ConstructorNormalizesDirectoryPathByRemovingTrailingForwardSlash()
+        {
+            var module = new Module("c:\\test/");
+            module.Directory.ShouldEqual("c:\\test");
+        }
+
+        [Fact]
+        public void ContainsPathOfAssetInModule_ReturnsTrue()
+        {
+            var module = new Module("c:\\test");
+            var asset = new Mock<IAsset>();
+            asset.SetupGet(a => a.SourceFilename).Returns("c:\\test\\asset.js");
+            module.Assets.Add(asset.Object);
+
+            module.ContainsPath("c:\\test\\asset.js").ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ContainsPathOfAssetInModuleWithDifferentCasing_ReturnsTrue()
+        {
+            var module = new Module("c:\\test");
+            var asset = new Mock<IAsset>();
+            asset.SetupGet(a => a.SourceFilename).Returns("c:\\test\\asset.js");
+            module.Assets.Add(asset.Object);
+
+            module.ContainsPath("c:\\TEST\\ASSET.js").ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ContainsPathOfAssetNotInModule_ReturnsFalse()
+        {
+            var module = new Module("c:\\test");
+
+            module.ContainsPath("c:\\test\\no-in-module.js").ShouldBeFalse();
+        }
+
+        [Fact]
+        public void ContainsPathOfJustTheModuleItself_ReturnsTrue()
+        {
+            var module = new Module("c:\\test");
+
+            module.ContainsPath("c:\\test").ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ContainsPathOfJustTheModuleItselfWithDifferentCasing_ReturnsTrue()
+        {
+            var module = new Module("c:\\test");
+
+            module.ContainsPath("c:\\TEST").ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ContainsPathOfJustTheModuleItselfWithTrailingSlash_ReturnsTrue()
+        {
+            var module = new Module("c:\\test");
+
+            module.ContainsPath("c:\\test\\").ShouldBeTrue();
+        }
+
+        [Fact]
         public void DisposeDisposesAllDisposableAssets()
         {
             var module = new Module("c:\\");
--- src/Cassette/Module.cs
+++ src/Cassette/Module.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace Cassette
 {
@@ -9,7 +10,7 @@ namespace Cassette
     {
         public Module(string directory)
         {
-            this.directory = directory;
+            this.directory = NormalizePath(directory);
         }
 
         readonly string directory;
@@ -23,6 +24,25 @@ namespace Cassette
         public IList<IAsset> Assets
         {
             get { return assets; }
+        }
+
+        public bool ContainsPath(string path)
+        {
+            if (IsModulePath(path)) return true;
+            return assets.Any(a => a.SourceFilename.Equals(path, StringComparison.OrdinalIgnoreCase));
+        }
+
+        bool IsModulePath(string path)
+        {
+            return directory.Equals(
+                NormalizePath(path),
+                StringComparison.OrdinalIgnoreCase
+            );
+        }
+
+        string NormalizePath(string path)
+        {
+            return path.TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
         }
 
         public void Dispose()
