717b55f42c4e3f888462704246b92f08586fc909
Andrew Davey <andrew@equin.co.uk>
2011-08-06 11:17:11 +0100

    Add ScriptModuleFactory basic tests.

---
+++ src/Cassette.UnitTests/ScriptModuleFactory.cs
@@ -0,0 +1,24 @@
+﻿using Should;
+using Xunit;
+
+namespace Cassette
+{
+    public class ScriptModuleFactory_Tests
+    {
+        [Fact]
+        public void CreateModuleReturnsScriptModule()
+        {
+            var factory = new ScriptModuleFactory();
+            var module = factory.CreateModule("c:\\test");
+            module.ShouldBeType<ScriptModule>();
+        }
+
+        [Fact]
+        public void CreateModuleAssignsScriptModuleDirectory()
+        {
+            var factory = new ScriptModuleFactory();
+            var module = factory.CreateModule("c:\\test");
+            module.Directory.ShouldEqual("c:\\test");
+        }
+    }
+}
