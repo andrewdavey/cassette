6c080f3de123c4f826a385b5a85f70733a4002dc
Andrew Davey <andrew@equin.co.uk>
2011-08-07 17:20:14 +0100

    Add IsolatedStorageFileSystem and integration test.

---
+++ src/Cassette.IntegrationTests/IsolatedStorageFileSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.IO.IsolatedStorage;
+using Should;
+using Xunit;
+
+namespace Cassette.IntegrationTests
+{
+    public class IsolatedStorageFileSystem_Tests
+    {
+        [Fact]
+        public void IsolatedStorageFileSystem_AccessIsolatedStorage()
+        {
+            using (var store = IsolatedStorageFile.GetMachineStoreForAssembly())
+            {
+                using (var writer = new StreamWriter(store.OpenFile("test.txt", FileMode.Create, FileAccess.Write)))
+                {
+                    writer.Write("test");
+                    writer.Flush();
+                }
+
+                var fileSystem = new IsolatedStorageFileSystem(store);
+                fileSystem.FileExists("test.txt").ShouldBeTrue();
+                using (var reader = new StreamReader(fileSystem.OpenFile("test.txt", FileMode.Open, FileAccess.Read)))
+                {
+                    reader.ReadToEnd().ShouldEqual("test");
+                }
+
+                fileSystem.DeleteAll();
+                store.FileExists("test.txt").ShouldBeFalse();
+            }
+        }
+    }
+}
---
+++ src/Cassette/IsolatedStorageFileSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace Cassette
+{
+    public class IsolatedStorageFileSystem : IFileSystem
+    {
+        public IsolatedStorageFileSystem(IsolatedStorageFile storage)
+        {
+            this.storage = storage;
+        }
+
+        readonly IsolatedStorageFile storage;
+
+        public bool FileExists(string filename)
+        {
+            return storage.FileExists(filename);
+        }
+
+        public Stream OpenFile(string filename, FileMode mode, FileAccess access)
+        {
+            return storage.OpenFile(filename, mode, access);
+        }
+
+        public void DeleteAll()
+        {
+            foreach (var filename in storage.GetFileNames())
+            {
+                storage.DeleteFile(filename);
+            }
+        }
+    }
+}
