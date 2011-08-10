727631e11930822a70180f50dd0c581d2712571a
Andrew Davey <andrew@equin.co.uk>
2011-08-10 17:46:40 +0100

    Add stylesheet module rendering and tests.

---
+++ src/Cassette.UnitTests/StylesheetModule.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Moq;
+using Should;
+using Xunit;
+
+namespace Cassette
+{
+    public class StylesheetModule_Render_Tests
+    {
+        public StylesheetModule_Render_Tests()
+        {
+            application = new Mock<ICassetteApplication>();
+            module = new StylesheetModule("test", Mock.Of<IFileSystem>());
+            asset1 = Mock.Of<IAsset>();
+            asset2 = Mock.Of<IAsset>();
+            module.Assets.Add(asset1);
+            module.Assets.Add(asset2);
+
+            application.Setup(a => a.CreateModuleUrl(module)).Returns("/url");
+            application.Setup(a => a.CreateAssetUrl(module, asset1)).Returns("/url1");
+            application.Setup(a => a.CreateAssetUrl(module, asset2)).Returns("/url2");
+        }
+
+        readonly Mock<ICassetteApplication> application;
+        readonly StylesheetModule module;
+        readonly IAsset asset1, asset2;
+        
+        [Fact]
+        public void GivenApplicationIsOutputOptimized_WhenRender_ThenModuleStylesheetLinkReturned()
+        {
+            application.SetupGet(a => a.IsOutputOptimized).Returns(true);
+
+            var html = module.Render(application.Object);
+
+            html.ToHtmlString().ShouldEqual(
+                "<link href=\"/url\" type=\"text/css\" rel=\"stylesheet\"/>"
+            );
+        }
+
+        [Fact]
+        public void GivenApplicationIsNotOutputOptimized_WhenRender_ThenAssetScriptsReturned()
+        {
+            application.SetupGet(a => a.IsOutputOptimized).Returns(false);
+
+            var html = module.Render(application.Object);
+
+            html.ToHtmlString().ShouldEqual(
+                "<link href=\"/url1\" type=\"text/css\" rel=\"stylesheet\"/>" + Environment.NewLine +
+                "<link href=\"/url2\" type=\"text/css\" rel=\"stylesheet\"/>"
+            );
+        }
+
+        [Fact]
+        public void GivenApplicationIsOutputOptimized_WhenMediaSetAndRender_ThenModuleLinksReturnedWithMediaAttribute()
+        {
+            application.SetupGet(a => a.IsOutputOptimized).Returns(true);
+
+            module.Media = "print";
+            var html = module.Render(application.Object);
+
+            html.ToHtmlString().ShouldEqual(
+                "<link href=\"/url\" type=\"text/css\" rel=\"stylesheet\" media=\"print\"/>"
+            );
+        }
+
+        [Fact]
+        public void GivenApplicationIsNotOutputOptimized_WhenMediaSetAndRender_ThenAssetLinksReturnedWithMediaAttribute()
+        {
+            application.SetupGet(a => a.IsOutputOptimized).Returns(false);
+
+            module.Media = "print";
+            var html = module.Render(application.Object);
+
+            html.ToHtmlString().ShouldEqual(
+                "<link href=\"/url1\" type=\"text/css\" rel=\"stylesheet\" media=\"print\"/>" + Environment.NewLine +
+                "<link href=\"/url2\" type=\"text/css\" rel=\"stylesheet\" media=\"print\"/>"
+            );
+        }
+    }
+}
--- src/Cassette/StylesheetModule.cs
+++ src/Cassette/StylesheetModule.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Web;
 
 namespace Cassette
 {
@@ -9,11 +11,41 @@ namespace Cassette
         {
         }
 
+        static readonly string linkHtml = "<link href=\"{0}\" type=\"text/css\" rel=\"stylesheet\"/>";
+        static readonly string linkHtmlWithMedia = "<link href=\"{0}\" type=\"text/css\" rel=\"stylesheet\" media=\"{1}\"/>";
+
         public string Media { get; set; }
 
         public override string ContentType
         {
             get { return "text/css"; }
+        }
+
+        public override IHtmlString Render(ICassetteApplication application)
+        {
+            if (application.IsOutputOptimized)
+            {
+                var url = application.CreateModuleUrl(this);
+                if (string.IsNullOrEmpty(Media))
+                {
+                    return new HtmlString(string.Format(linkHtml, url));
+                }
+                else
+                {
+                    return new HtmlString(string.Format(linkHtmlWithMedia, url, Media));
+                }
+            }
+            else
+            {
+                var hasMedia = string.IsNullOrEmpty(Media);
+                var scripts = string.Join(Environment.NewLine,
+                    from asset in Assets
+                    let url = application.CreateAssetUrl(this, asset)
+                    select hasMedia ? string.Format(linkHtml, url) 
+                                    : string.Format(linkHtmlWithMedia, url, Media)
+                );
+                return new HtmlString(scripts);
+            }
         }
     }
 }
