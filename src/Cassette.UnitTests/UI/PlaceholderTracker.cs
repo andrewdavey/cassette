673389e4fc838799ae95ab76400c6d0e72ec749c
Andrew Davey <andrew@equin.co.uk>
2011-08-11 21:39:00 +0100

    Add PlaceholderTracker.

---
+++ src/Cassette.UnitTests/UI/PlaceholderTracker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web;
+using Should;
+using Xunit;
+
+namespace Cassette.UI
+{
+    public class PlaceholderTracker_Tests
+    {
+        [Fact]
+        public void WhenInsertPlaceholder_ThenPlaceholderIdHtmlReturned()
+        {
+            var tracker = new PlaceholderTracker();
+            var result = tracker.InsertPlaceholder("id", () => new HtmlString(""));
+            result.ToHtmlString().ShouldEqual(
+                Environment.NewLine + "id" + Environment.NewLine
+            );
+        }
+
+        [Fact]
+        public void GivenInsertPlaceholder_WhenReplacePlaceholders_ThenHtmlInserted()
+        {
+            var tracker = new PlaceholderTracker();
+            var html = tracker.InsertPlaceholder("id", () => new HtmlString("<p>test</p>"));
+
+            tracker.ReplacePlaceholders(html.ToHtmlString()).ShouldEqual(
+                Environment.NewLine + "<p>test</p>" + Environment.NewLine
+            );
+        }
+    }
+}
---
+++ src/Cassette/UI/PlaceholderTracker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+
+namespace Cassette.UI
+{
+    public class PlaceholderTracker
+    {
+        readonly Dictionary<string, Func<IHtmlString>> creationFunctions = new Dictionary<string, Func<IHtmlString>>();
+
+        public IHtmlString InsertPlaceholder(string id, Func<IHtmlString> createHtml)
+        {
+            creationFunctions[id] = createHtml;
+            return new HtmlString(Environment.NewLine + id + Environment.NewLine);
+        }
+
+        public string ReplacePlaceholders(string html)
+        {
+            var builder = new StringBuilder(html);
+            foreach (var item in creationFunctions)
+            {
+                builder.Replace(item.Key, item.Value().ToHtmlString());
+            }
+            return builder.ToString();
+        }
+    }
+}
