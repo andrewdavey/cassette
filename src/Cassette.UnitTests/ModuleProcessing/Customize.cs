7da778a1710a2e5cc86da9217910127cf29a7c94
Andrew Davey <andrew@equin.co.uk>
2011-08-12 15:24:11 +0100

    Add module Customize step.

---
+++ src/Cassette.UnitTests/ModuleProcessing/Customize.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Moq;
+using Should;
+using Xunit;
+
+namespace Cassette.ModuleProcessing
+{
+    public class Customize_Tests
+    {
+        [Fact]
+        public void ProcessRunsActionForModule()
+        {
+            var step = new Customize<StylesheetModule>(
+                m => m.Media = "print"    
+            );
+            var module = new StylesheetModule("test", Mock.Of<IFileSystem>());
+
+            step.Process(module, Mock.Of<ICassetteApplication>());
+
+            module.Media.ShouldEqual("print");
+        }
+
+        [Fact]
+        public void ActionCannotBeNull()
+        {
+            Assert.Throws<ArgumentNullException>(delegate
+            {
+                new Customize<Module>(null);
+            });
+        }
+    }
+}
---
+++ src/Cassette/ModuleProcessing/Customize.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cassette.ModuleProcessing
+{
+    public class Customize<T> : IModuleProcessor<T>
+        where T : Module
+    {
+        public Customize(Action<T> action)
+        {
+            if (action == null) throw new ArgumentNullException("action");
+            this.action = action;
+        }
+
+        readonly Action<T> action;
+
+        public void Process(T module, ICassetteApplication application)
+        {
+            action(module);
+        }
+    }
+}
