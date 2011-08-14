1e46b37c763a22c00dc91829366eef889369cd54
Andrew Davey <andrew@equin.co.uk>
2011-08-14 17:46:28 +0100

    Add default stylesheet and html template pipelines.

---
+++ src/Cassette/DefaultHtmlTemplatePipeline.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Cassette.HtmlTemplates;
+using Cassette.ModuleProcessing;
+
+namespace Cassette
+{
+    public class DefaultHtmlTemplatePipeline : IModuleProcessor<HtmlTemplateModule>
+    {
+        public void Process(HtmlTemplateModule module, ICassetteApplication application)
+        {
+            foreach (var processor in CreatePipeline(application))
+            {
+                processor.Process(module, application);
+            }
+        }
+
+        IEnumerable<IModuleProcessor<HtmlTemplateModule>> CreatePipeline(ICassetteApplication application)
+        {
+            yield return new WrapHtmlTemplatesInScriptBlocks();
+            if (application.IsOutputOptimized)
+            {
+                yield return new ConcatenateAssets();
+            }
+        }
+    }
+}
---
+++ src/Cassette/DefaultStylesheetPipeline.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Cassette.ModuleProcessing;
+using Cassette.Stylesheets;
+
+namespace Cassette
+{
+    public class DefaultStylesheetPipeline : IModuleProcessor<StylesheetModule>
+    {
+        public DefaultStylesheetPipeline()
+        {
+            StylesheetMinifier = new MicrosoftStyleSheetMinifier();
+        }
+
+        public IAssetTransformer StylesheetMinifier { get; set; }
+        public bool CompileLess { get; set; }
+
+        public void Process(StylesheetModule module, ICassetteApplication application)
+        {
+            foreach (var processor in CreatePipeline(application))
+            {
+                processor.Process(module, application);
+            }
+        }
+
+        IEnumerable<IModuleProcessor<StylesheetModule>> CreatePipeline(ICassetteApplication application)
+        {
+            yield return new ParseCssReferences();
+            if (CompileLess)
+            {
+                yield return new ParseLessReferences();
+                yield return new CompileLess(new LessCompiler());
+            }
+            yield return new SortAssetsByDependency();
+            if (application.IsOutputOptimized)
+            {
+                yield return new ConcatenateAssets();
+                yield return new MinifyAssets(StylesheetMinifier);
+            }
+        }
+    }
+}
