36ab9489d83c4d6f1d8a86aeef86f453a77f5028
Andrew Davey <andrew@equin.co.uk>
2011-08-15 09:26:41 +0100

    Test external script module and add fallback script rendering.

---
+++ src/Cassette.UnitTests/Scripts/ExternalScriptModule.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Moq;
+using Should;
+using Xunit;
+
+namespace Cassette.Scripts
+{
+    public class ExternalScriptModule_Tests
+    {
+        [Fact]
+        public void GivenExternalScriptModuleWithFallback_ThenRenderReturnsFallbackScript()
+        {
+            var module = new ExternalScriptModule("api", "http://test.com/api.js", "/api.js", "!window.api");
+            var html = module.Render(Mock.Of<ICassetteApplication>());
+            html.ToHtmlString().ShouldEqual(
+                @"<script src=""http://test.com/api.js"" type=""text/javascript""></script>" + 
+                Environment.NewLine + 
+                @"<script type=""text/javascript"">!window.api && document.write(unescape('%3Cscript src=""/api.js""%3E%3C/script%3E'))</script>"
+            );
+        }
+    }
+
+    public class ExternalScriptModule_ConstructorConstraints
+    {
+        [Fact]
+        public void UrlRequired()
+        {
+            Assert.Throws<ArgumentNullException>(delegate
+            {
+                new ExternalScriptModule("api", null, "/api.js", "!window.api");
+            });
+            Assert.Throws<ArgumentException>(delegate
+            {
+                new ExternalScriptModule("api", "", "/api.js", "!window.api");
+            });
+            Assert.Throws<ArgumentException>(delegate
+            {
+                new ExternalScriptModule("api", " ", "/api.js", "!window.api");
+            });
+        }
+
+        [Fact]
+        public void FallbackUrlRequired()
+        {
+            Assert.Throws<ArgumentNullException>(delegate
+            {
+                new ExternalScriptModule("api", "http://test.com/api.js", null, "!window.api");
+            });
+            Assert.Throws<ArgumentException>(delegate
+            {
+                new ExternalScriptModule("api", "http://test.com/api.js", "", "!window.api");
+            });
+            Assert.Throws<ArgumentException>(delegate
+            {
+                new ExternalScriptModule("api", "http://test.com/api.js", " ", "!window.api");
+            });
+        }
+
+        [Fact]
+        public void JavaScriptConditionRequired()
+        {
+            Assert.Throws<ArgumentNullException>(delegate
+            {
+                new ExternalScriptModule("api", "http://test.com/api.js", "/api.js", null);
+            });
+            Assert.Throws<ArgumentException>(delegate
+            {
+                new ExternalScriptModule("api", "http://test.com/api.js", "/api.js", "");
+            });
+            Assert.Throws<ArgumentException>(delegate
+            {
+                new ExternalScriptModule("api", "http://test.com/api.js", "/api.js", " ");
+            });
+        }
+    }
+}
--- src/Cassette/Scripts/ExternalScriptModule.cs
+++ src/Cassette/Scripts/ExternalScriptModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 
 namespace Cassette.Scripts
 {
@@ -7,14 +8,53 @@ namespace Cassette.Scripts
         public ExternalScriptModule(string url)
             : base("")
         {
+            if (url == null) throw new ArgumentNullException("url");
+            if (string.IsNullOrWhiteSpace(url)) throw new ArgumentException("URL is required.", "url");
+            
             this.url = url;
         }
 
-        readonly string url;
+        public ExternalScriptModule(string name, string url, string fallbackUrl, string javaScriptCondition)
+            : base(name)
+        {
+            if (url == null) throw new ArgumentNullException("url");
+            if (string.IsNullOrWhiteSpace(url)) throw new ArgumentException("URL is required.", "url");
+            if (fallbackUrl == null) throw new ArgumentNullException("fallbackUrl");
+            if (string.IsNullOrWhiteSpace(fallbackUrl)) throw new ArgumentException("Fallback URL is required.", "fallbackUrl");
+            if (javaScriptCondition == null) throw new ArgumentNullException("javaScriptCondition");
+            if (string.IsNullOrWhiteSpace(javaScriptCondition)) throw new ArgumentException("JavaScript condition is required.", "javaScriptCondition");
 
+            this.url = url;
+            this.fallbackUrl = fallbackUrl;
+            this.javaScriptCondition = javaScriptCondition;
+        }
+
+        readonly string url;
+        readonly string fallbackUrl;
+        readonly string javaScriptCondition;
+
+        static readonly string fallbackHtml = "<script type=\"text/javascript\">{0} && document.write(unescape('%3Cscript src=\"{1}\"%3E%3C/script%3E'))</script>";
+        
         public override IHtmlString Render(ICassetteApplication application)
         {
-            return new HtmlString(string.Format(scriptHtml, url));
+            if (string.IsNullOrEmpty(fallbackUrl))
+            {
+                return new HtmlString(
+                    string.Format(scriptHtml, url)
+                );
+            }
+            else
+            {
+                return new HtmlString(
+                    string.Format(scriptHtml, url) + 
+                    Environment.NewLine + 
+                    string.Format(
+                        fallbackHtml,
+                        javaScriptCondition,
+                        fallbackUrl
+                    )
+                );
+            }
         }
     }
 }
