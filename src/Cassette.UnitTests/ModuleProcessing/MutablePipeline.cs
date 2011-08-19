62e574296242ff6ed020874c0ffd1f62103e5e5a
Andrew Davey <andrew@equin.co.uk>
2011-08-19 22:35:05 +0100

    Add mutable pipeline.

---
+++ src/Cassette.UnitTests/ModuleProcessing/MutablePipeline.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections.Generic;
+using Moq;
+using Should;
+using Xunit;
+
+namespace Cassette.ModuleProcessing
+{
+    public class MutablePipeline_Tests
+    {
+        [Fact]
+        public void ProcessCallsCreatePipeline()
+        {
+            var pipeline = new MockPipeline<Module>();
+            pipeline.Process(new Module(""), Mock.Of<ICassetteApplication>());
+            pipeline.CreatePipelineCalled.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ProcessCallsStep()
+        {
+            var pipeline = new MockPipeline<Module>();
+            pipeline.Process(new Module(""), Mock.Of<ICassetteApplication>());
+            pipeline.DummyStep.ProcessCalled.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void WhenRemoveStep_ThenProcessDoesNotCallIt()
+        {
+            var pipeline = new MockPipeline<Module>();
+            pipeline.Remove<MockStep>();
+
+            pipeline.Process(new Module(""), Mock.Of<ICassetteApplication>());
+
+            pipeline.DummyStep.ProcessCalled.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void WhenReplaceStep_ThenProcessCallsNewStep()
+        {
+            var pipeline = new MockPipeline<Module>();
+            var newStep = new MockStep();
+            pipeline.Replace<MockStep>(newStep);
+
+            pipeline.Process(new Module(""), Mock.Of<ICassetteApplication>());
+
+            newStep.ProcessCalled.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void WhenReplaceStep_ThenProcessDoesNotCallOldStep()
+        {
+            var pipeline = new MockPipeline<Module>();
+            var newStep = new MockStep();
+            pipeline.Replace<MockStep>(newStep);
+
+            pipeline.Process(new Module(""), Mock.Of<ICassetteApplication>());
+
+            pipeline.DummyStep.ProcessCalled.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void WhenInsertAfterStep_TheProcessCallsTheNewStep()
+        {
+            var pipeline = new MockPipeline<Module>();
+            var newStep = new MockStep();
+            pipeline.InsertAfter<MockStep>(newStep);
+
+            pipeline.Process(new Module(""), Mock.Of<ICassetteApplication>());
+
+            newStep.ProcessCalled.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void WhenInsertAfterStep_TheProcessCallsTheNewStepAfterOriginalStep()
+        {
+            var pipeline = new MockPipeline<Module>();
+            var newStep = new MockStep();
+            pipeline.InsertAfter<MockStep>(newStep);
+
+            pipeline.Process(new Module(""), Mock.Of<ICassetteApplication>());
+
+            (newStep.CallIndex > pipeline.DummyStep.CallIndex).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void WhenInsertBeforeStep_TheProcessCallsTheNewStep()
+        {
+            var pipeline = new MockPipeline<Module>();
+            var newStep = new MockStep();
+            pipeline.InsertBefore<MockStep>(newStep);
+
+            pipeline.Process(new Module(""), Mock.Of<ICassetteApplication>());
+
+            newStep.ProcessCalled.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void WhenInsertBeforeStep_TheProcessCallsTheNewStepBeforeOriginalStep()
+        {
+            var pipeline = new MockPipeline<Module>();
+            var newStep = new MockStep();
+            pipeline.InsertBefore<MockStep>(newStep);
+
+            pipeline.Process(new Module(""), Mock.Of<ICassetteApplication>());
+
+            (newStep.CallIndex < pipeline.DummyStep.CallIndex).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void WhenUpdateStep_ThenProcessCallsUpdateAction()
+        {
+            var pipeline = new MockPipeline<Module>();
+            pipeline.Update<MockStep>(step => step.Updated = true);
+
+            pipeline.Process(new Module(""), Mock.Of<ICassetteApplication>());
+
+            pipeline.DummyStep.Updated.ShouldBeTrue();
+        }
+
+        class MockPipeline<T> : MutablePipeline<T>
+            where T : Module
+        {
+            protected override IEnumerable<IModuleProcessor<T>> CreatePipeline(T module, ICassetteApplication application)
+            {
+                CreatePipelineCalled = true;
+                yield return DummyStep;
+            }
+
+            public MockStep DummyStep = new MockStep();
+            public bool CreatePipelineCalled;
+        }
+
+        class MockStep : IModuleProcessor<Module>
+        {
+            public void Process(Module module, ICassetteApplication application)
+            {
+                CallIndex = count++;
+                ProcessCalled = true;
+            }
+
+            public bool ProcessCalled;
+            public int CallIndex;
+            public bool Updated;
+
+            static int count;
+        }
+    }
+}
---
+++ src/Cassette/ModuleProcessing/MutablePipeline.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cassette.ModuleProcessing
+{
+    public abstract class MutablePipeline<T> : IModuleProcessor<T>
+        where T : Module
+    {
+        readonly List<Func<IEnumerable<IModuleProcessor<T>>, IEnumerable<IModuleProcessor<T>>>> pipelineModifiers = new List<Func<IEnumerable<IModuleProcessor<T>>, IEnumerable<IModuleProcessor<T>>>>();
+
+        public void Process(T module, ICassetteApplication application)
+        {
+            var steps = pipelineModifiers.Aggregate(
+                CreatePipeline(module, application),
+                (pipeline, modify) => modify(pipeline)
+            );
+            foreach (var step in steps)
+            {
+                step.Process(module, application);
+            }
+        }
+
+        public void Remove<TStep>()
+            where TStep : IModuleProcessor<T>
+        {
+            pipelineModifiers.Add(
+                steps => steps.Where(step => (step is TStep) == false)
+            );
+        }
+
+        public void Replace<TStep>(IModuleProcessor<T> newStep)
+            where TStep : IModuleProcessor<T>
+        {
+            pipelineModifiers.Add(
+                steps => steps.Select(
+                    step => step is TStep ? newStep : step
+                )
+            );
+        }
+
+        public void InsertAfter<TStep>(IModuleProcessor<T> newStep)
+            where TStep : IModuleProcessor<T>
+        {
+            pipelineModifiers.Add(
+                steps => steps.SelectMany(
+                    step => step is TStep 
+                        ? new[] { step, newStep } 
+                        : new[] { step }
+                )
+            );
+        }
+
+        public void InsertBefore<TStep>(IModuleProcessor<T> newStep)
+            where TStep : IModuleProcessor<T>
+        {
+            pipelineModifiers.Add(
+                steps => steps.SelectMany(
+                    step => step is TStep
+                        ? new[] { newStep, step }
+                        : new[] { step }
+                )
+            );
+        }
+
+        public void Update<TStep>(Action<TStep> update)
+            where TStep : IModuleProcessor<T>
+        {
+            pipelineModifiers.Add(
+                steps =>
+                {
+                    foreach (var step in steps.OfType<TStep>())
+                    {
+                        update(step);
+                    }
+                    return steps;
+                }
+            );
+        }
+
+        protected abstract IEnumerable<IModuleProcessor<T>> CreatePipeline(T module, ICassetteApplication application);
+    }
+}
