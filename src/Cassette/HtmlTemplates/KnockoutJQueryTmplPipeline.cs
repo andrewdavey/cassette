f8091ef3cbe8e681e5dad20c52fa28b448770dd0
Andrew Davey <andrew@equin.co.uk>
2011-08-21 10:59:46 +0100

    Create separate pipeline class for the Knockout tmpl compiler.

--- src/Cassette/HtmlTemplates/KnockoutJQueryTmplPipeline.cs
+++ src/Cassette/HtmlTemplates/KnockoutJQueryTmplPipeline.cs
@@ -3,11 +3,11 @@ using Cassette.ModuleProcessing;
 
 namespace Cassette.HtmlTemplates
 {
-    public class JQueryTmplPipeline : MutablePipeline<HtmlTemplateModule>
+    public class KnockoutJQueryTmplPipeline : MutablePipeline<HtmlTemplateModule>
     {
         protected override IEnumerable<IModuleProcessor<HtmlTemplateModule>> CreatePipeline(HtmlTemplateModule module, ICassetteApplication application)
         {
-            yield return new AddTransformerToAssets(new CompileAsset(new JQueryTmplCompiler()));
+            yield return new AddTransformerToAssets(new CompileAsset(new KnockoutJQueryTmplCompiler()));
             yield return new ConcatenateAssets();
             yield return new AddTransformerToAssets(new WrapWithJavaScriptElement());
         }
