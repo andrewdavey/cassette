3c4b5118b6d18083bf8f87276c792ad52cfcbb3d
Andrew Davey <andrew@equin.co.uk>
2011-08-22 16:11:15 +0100

    Add CassetteApplicationContainer.

---
+++ src/Cassette.UnitTests/CassetteApplicationContainer.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Moq;
+using Should;
+using Xunit;
+
+namespace Cassette
+{
+    public class CassetteApplicationContainer_Tests : IDisposable
+    {
+        public CassetteApplicationContainer_Tests()
+        {
+            rootPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(rootPath);
+            applicationInstances = new[] {CreateApplication(), CreateApplication()};
+        }
+
+        [Fact]
+        public void WhenCreateContainer_ThenApplicationPropertyReturnsApplicationCreatedByFactoryMethod()
+        {
+            using (var container = new CassetteApplicationContainer<ICassetteApplication>(StubApplicationFactory))
+            {
+                container.Application.ShouldBeSameAs(applicationInstances[0].Object);
+            }
+        }
+
+        [Fact]
+        public void WhenFileCreated_TheApplicationFactoryCalledAgain()
+        {
+            using (var container = new CassetteApplicationContainer<ICassetteApplication>(StubApplicationFactory))
+            {
+                File.WriteAllText(Path.Combine(rootPath, "test.txt"), "");
+                Thread.Sleep(500); // File system events are asynchronously fired on different thread. Hack a brief pause to catch the event!
+                var getItAgain = container.Application;
+            }
+            factoryCallCount.ShouldEqual(2);
+        }
+
+        [Fact]
+        public void WhenFileCreated_ApplicationPropertyReturnsSecondApplication()
+        {
+            using (var container = new CassetteApplicationContainer<ICassetteApplication>(StubApplicationFactory))
+            {
+                File.WriteAllText(Path.Combine(rootPath, "test.txt"), "");
+                Thread.Sleep(500); // File system events are asynchronously fired on different thread. Hack a brief pause to catch the event!
+                container.Application.ShouldBeSameAs(applicationInstances[1].Object);
+            }
+        }
+
+        [Fact]
+        public void WhenFileDeleted_TheApplicationFactoryCalledAgain()
+        {
+            var filename = Path.Combine(rootPath, "test.txt");
+            File.WriteAllText(filename, "");
+            using (var container = new CassetteApplicationContainer<ICassetteApplication>(StubApplicationFactory))
+            {
+                File.Delete(filename);
+                Thread.Sleep(500); // File system events are asynchronously fired on different thread. Hack a brief pause to catch the event!
+                var getItAgain = container.Application;
+            }
+            factoryCallCount.ShouldEqual(2);
+        }
+
+        [Fact]
+        public void WhenFileChanged_TheApplicationFactoryCalledAgain()
+        {
+            var filename = Path.Combine(rootPath, "test.txt");
+            File.WriteAllText(filename, "");
+            using (var container = new CassetteApplicationContainer<ICassetteApplication>(StubApplicationFactory))
+            {
+                File.WriteAllText(filename, "changed");
+                Thread.Sleep(500); // File system events are asynchronously fired on different thread. Hack a brief pause to catch the event!
+                var getItAgain = container.Application;
+            }
+            factoryCallCount.ShouldEqual(2);
+        }
+
+        [Fact]
+        public void WhenFileRenamed_TheApplicationFactoryCalledAgain()
+        {
+            var filename = Path.Combine(rootPath, "test.txt");
+            File.WriteAllText(filename, "");
+            using (var container = new CassetteApplicationContainer<ICassetteApplication>(StubApplicationFactory))
+            {
+                File.Move(filename, filename + ".new");
+                Thread.Sleep(500); // File system events are asynchronously fired on different thread. Hack a brief pause to catch the event!
+                var getItAgain = container.Application;
+            }
+            factoryCallCount.ShouldEqual(2);
+        }
+
+        [Fact]
+        public void WhenCreatingANewApplication_ThenOldApplicationIsDisposed()
+        {
+            var filename = Path.Combine(rootPath, "test.txt");
+            using (var container = new CassetteApplicationContainer<ICassetteApplication>(StubApplicationFactory))
+            {
+                File.WriteAllText(filename, "");
+                Thread.Sleep(500);
+                    // File system events are asynchronously fired on different thread. Hack a brief pause to catch the event!
+                var getItAgain = container.Application;
+            }
+            applicationInstances[0].Verify(a => a.Dispose());
+        }
+
+        [Fact]
+        public void WhenDispose_ThenCurrentApplicationInstanceDisposed()
+        {
+            using (var container = new CassetteApplicationContainer<ICassetteApplication>(StubApplicationFactory))
+            {
+                
+            }
+            applicationInstances[0].Verify(a => a.Dispose());
+        }
+
+        readonly Mock<ICassetteApplication>[] applicationInstances;
+        readonly string rootPath;
+        int factoryCallCount;
+
+        Mock<ICassetteApplication> CreateApplication()
+        {
+            var app = new Mock<ICassetteApplication>();
+            var directory = new Mock<IFileSystem>();
+            directory.Setup(d => d.GetAbsolutePath(""))
+                     .Returns(rootPath);
+            app.SetupGet(a => a.RootDirectory).Returns(directory.Object);
+            return app;
+        }
+
+        ICassetteApplication StubApplicationFactory()
+        {
+            return applicationInstances[factoryCallCount++].Object;
+        }
+
+        public void Dispose()
+        {
+            Directory.Delete(rootPath, true);
+        }
+    }
+}
---
+++ src/Cassette/CassetteApplicationContainer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+
+namespace Cassette
+{
+    public class CassetteApplicationContainer<T> : IDisposable
+    {
+        readonly Func<ICassetteApplication> createApplication;
+        FileSystemWatcher watcher;
+        Lazy<ICassetteApplication> application;
+
+        public CassetteApplicationContainer(Func<ICassetteApplication> createApplication)
+        {
+            this.createApplication = createApplication;
+            application = new Lazy<ICassetteApplication>(createApplication);
+
+            // In production mode we don't expect the asset files to change
+            // while the application is running. Changes to assets will involve a 
+            // re-deployment and restart of the app pool. So new assets are loaded then.
+
+            // In development mode, asset files will likely change while application is
+            // running. So watch the file system and recycle the application object 
+            // when files are created/changed/deleted/etc.
+            var shouldWatchFileSystem = application.Value.IsOutputOptimized == false;
+            if (shouldWatchFileSystem)
+            {
+                StartWatchingFileSystem();
+            }
+        }
+
+        public ICassetteApplication Application
+        {
+            get
+            {
+                return application.Value;
+            }
+        }
+
+        void StartWatchingFileSystem()
+        {
+            var rootDirectory = application.Value.RootDirectory.GetAbsolutePath("");
+            watcher = new FileSystemWatcher(rootDirectory)
+            {
+                IncludeSubdirectories = true,
+                EnableRaisingEvents = true
+            };
+            watcher.Created += RecycleApplication;
+            watcher.Changed += RecycleApplication;
+            watcher.Renamed += RecycleApplication;
+            watcher.Deleted += RecycleApplication;
+        }
+
+        void RecycleApplication(object sender, FileSystemEventArgs e)
+        {
+            if (application.IsValueCreated == false) return; // Already recycled, awaiting first creation.
+
+            lock (this)
+            {
+                if (!application.IsValueCreated) return;
+
+                application.Value.Dispose();
+                // Re-create the lazy object. So the application isn't created until it's asked for.
+                application = new Lazy<ICassetteApplication>(createApplication);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (watcher != null)
+            {
+                watcher.Dispose();
+            }
+            if (application.IsValueCreated)
+            {
+                application.Value.Dispose();
+            }
+        }
+    }
+}
