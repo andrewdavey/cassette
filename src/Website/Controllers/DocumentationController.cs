472bfb5ab49544b82973ef9b604111865e20cb19
Andrew Davey <andrew@equin.co.uk>
2011-08-26 13:12:06 +0100

    Temporary config change for error reporting.

---
+++ src/Website/Controllers/DocumentationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website.Controllers
+{
+    public class DocumentationController : Controller
+    {
+        //
+        // GET: /Documentation/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
