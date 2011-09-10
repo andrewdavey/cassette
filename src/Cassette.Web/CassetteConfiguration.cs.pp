1d3beb38c5295f273e179b3bfe03320c3f1d6941
Andrew Davey <andrew@equin.co.uk>
2011-09-10 17:19:16 +0100

    Clean up the template configuration file.

--- src/Cassette.Web/CassetteConfiguration.cs.pp
+++ src/Cassette.Web/CassetteConfiguration.cs.pp
@@ -14,26 +14,6 @@ namespace $rootnamespace$
 
             // *** TOP TIP: Delete all ".min.js" files now ***
             // Cassette minifies scripts for you. So those files are never used.
-
-            // Without a configuration, Cassette uses a simple set of defaults:
-            //
-            // Scripts
-            //    Single module from the application's root directory
-            //    Files included: *.js and *.coffee
-            //    Files excluded: File names ending with "-vsdoc.js"
-            //    CoffeeScript compilation enabled
-            //
-            // Stylesheets
-            //    Single module from the application's root directory
-            //    Files included: *.css and *.less
-            //    File excluded: none
-            //    LESS compilation enabled
-            //
-            // HTML Templates
-            //    Single module from the application's root directory
-            //    Files included: *.htm and *.html
-            //    Files excluded: none
-            //    Compilation disabled
         }
     }
 }
