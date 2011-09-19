2b621c150049f53e6ce1ee7790b60027e4982bff
Andrew Davey <andrew@equin.co.uk>
2011-09-19 11:06:17 +0100

    Add FileSystemFile tests.

---
+++ src/Cassette.UnitTests/IO/FileSystemFile.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using Cassette.Utilities;
+using Moq;
+using Should;
+using Xunit;
+
+namespace Cassette.IO
+{
+    public class FileSystemFile_Tests : IDisposable
+    {
+        readonly string filename;
+        readonly IDirectory directory;
+        readonly FileSystemFile file;
+
+        public FileSystemFile_Tests()
+        {
+            filename = Path.GetTempFileName();
+            directory = Mock.Of<IDirectory>();
+            file = new FileSystemFile(filename, directory);
+
+            File.WriteAllText(filename, "test");
+        }
+
+        [Fact]
+        public void DirectoryReturnsDirectoryPassedToConstructor()
+        {
+            file.Directory.ShouldBeSameAs(directory);
+        }
+
+        [Fact]
+        public void FullPathReturnsFilenamePassedToConstructor()
+        {
+            file.FullPath.ShouldEqual(filename);
+        }
+
+        [Fact]
+        public void ExistsReturnsTrue()
+        {
+            file.Exists.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void LastWriteTimeUtcReturnsLastWriteTimeOfActualFile()
+        {
+            file.LastWriteTimeUtc.ShouldEqual(File.GetLastWriteTimeUtc(filename));
+        }
+
+        [Fact]
+        public void OpenReturnsFileStream()
+        {
+            using (var stream = file.Open(FileMode.Open, FileAccess.Read))
+            {
+                stream.ReadToEnd().ShouldEqual("test");
+            }
+        }
+
+        public void Dispose()
+        {
+            File.Delete(filename);
+        }
+    }
+}
