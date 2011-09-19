3282bf6ace15aef56f1327b6b4b3d0bd99e0f471
Andrew Davey <andrew@equin.co.uk>
2011-09-19 10:59:48 +0100

    Add NonExistentFile tests.

---
+++ src/Cassette.UnitTests/IO/NonExistentFile.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using Should;
+using Xunit;
+
+namespace Cassette.IO
+{
+    public class NonExistentFile_Tests
+    {
+        readonly NonExistentFile file = new NonExistentFile("c:\\fail.txt");
+
+        [Fact]
+        public void DirectoryPropertyThrowsFileNotFoundException()
+        {
+            Assert.Throws<FileNotFoundException>(() => file.Directory);
+        }
+
+        [Fact]
+        public void LastWriteTimeUtcThrowsFileNotFoundException()
+        {
+            Assert.Throws<FileNotFoundException>(() => file.LastWriteTimeUtc);
+        }
+
+        [Fact]
+        public void OpenThrowsFileNotFoundException()
+        {
+            Assert.Throws<FileNotFoundException>(() => file.Open(FileMode.Open, FileAccess.Read));
+        }
+
+        [Fact]
+        public void ExistsReturnsFalse()
+        {
+            file.Exists.ShouldEqual(false);
+        }
+
+        [Fact]
+        public void FullPathReturnsPathPassedToConstructor()
+        {
+            file.FullPath.ShouldEqual("c:\\fail.txt");
+        }
+    }
+}
