7147e677506911510857ccaa46d227215a0b2752
Andrew Davey <andrew@equin.co.uk>
2011-09-30 09:21:39 +0100

    Add DelegateRouteHandler.

---
+++ src/Cassette.UnitTests/Web/DelegateRouteHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Web;
+using System.Web.Routing;
+using Moq;
+using Should;
+using Xunit;
+
+namespace Cassette.Web
+{
+    public class DelegateRouteHandler_Tests
+    {
+        [Fact]
+        public void GetHttpHandlerCallsDelegatePassingInTheRequestContext()
+        {
+            RequestContext actualContext = null;
+            var stubHandler = Mock.Of<IHttpHandler>();
+            var stubContext = Mock.Of<RequestContext>();
+
+            var routeHandler = new DelegateRouteHandler(context => {
+                actualContext = context;
+                return stubHandler;
+            });
+
+            var actualHttpHandler = routeHandler.GetHttpHandler(stubContext);
+
+            actualHttpHandler.ShouldBeSameAs(stubHandler);
+            actualContext.ShouldBeSameAs(stubContext);
+        }
+    }
+}
---
+++ src/Cassette.Web/DelegateRouteHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+using System.Web.Routing;
+
+namespace Cassette.Web
+{
+    public class DelegateRouteHandler : IRouteHandler
+    {
+        readonly Func<RequestContext, IHttpHandler> createHandler;
+
+        public DelegateRouteHandler(Func<RequestContext, IHttpHandler> createHandler)
+        {
+            this.createHandler = createHandler;
+        }
+
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return createHandler(requestContext);
+        }
+    }
+}
