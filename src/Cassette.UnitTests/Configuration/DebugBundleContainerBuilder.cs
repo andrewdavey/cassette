29d786ec57bbee1d6d1cc2e9a5c32362ebe342ec
Andrew Davey <andrew@equin.co.uk>
2011-10-11 15:09:33 +0100

    Start optimized bundle container factory.

--- src/Cassette.UnitTests/Configuration/DebugBundleContainerBuilder.cs
+++ src/Cassette.UnitTests/Configuration/DebugBundleContainerBuilder.cs
@@ -5,6 +5,7 @@ using Cassette.Utilities;
 using Moq;
 using Should;
 using Xunit;
+using Cassette.Persistence;
 
 namespace Cassette.Configuration
 {
@@ -18,8 +19,8 @@ namespace Cassette.Configuration
             var bundles = new [] { bundle1, bundle2 };
             var application = Mock.Of<ICassetteApplication>();
 
-            var builder = new DebugBundleContainerBuilder(application, new Dictionary<Type, IBundleFactory<Bundle>>());
-            var container = builder.Build(bundles);
+            var builder = new BundleContainerFactory(application, new Dictionary<Type, IBundleFactory<Bundle>>());
+            var container = builder.Create(bundles);
 
             container.FindBundleContainingPath("~/test1").ShouldBeSameAs(bundle1);
             container.FindBundleContainingPath("~/test2").ShouldBeSameAs(bundle2);
@@ -31,8 +32,8 @@ namespace Cassette.Configuration
             var bundle = new Mock<Bundle>("~/test");
             var application = Mock.Of<ICassetteApplication>();
 
-            var builder = new DebugBundleContainerBuilder(application, new Dictionary<Type, IBundleFactory<Bundle>>());
-            builder.Build(new[] { bundle.Object });
+            var builder = new BundleContainerFactory(application, new Dictionary<Type, IBundleFactory<Bundle>>());
+            builder.Create(new[] { bundle.Object });
 
             bundle.Verify(b => b.Process(application));
         }
@@ -51,8 +52,8 @@ namespace Cassette.Configuration
                    .Returns(externalBundle);
             factories[typeof(Bundle)] = factory.Object;
 
-            var builder = new DebugBundleContainerBuilder(application, factories);
-            var container = builder.Build(new[] { bundle });
+            var builder = new BundleContainerFactory(application, factories);
+            var container = builder.Create(new[] { bundle });
 
             container.FindBundleContainingPath("http://external.com/api.js").ShouldBeSameAs(externalBundle);
         }
@@ -74,25 +75,25 @@ namespace Cassette.Configuration
                    .Returns(externalBundle);
             factories[typeof(Bundle)] = factory.Object;
 
-            var builder = new DebugBundleContainerBuilder(application, factories);
-            var container = builder.Build(new[] { bundle1, bundle2 });
+            var builder = new BundleContainerFactory(application, factories);
+            var container = builder.Create(new[] { bundle1, bundle2 });
 
             container.Bundles.Count().ShouldEqual(3);
         }
     }
 
-    class DebugBundleContainerBuilder
+    class BundleContainerFactory
     {
         readonly ICassetteApplication application;
         readonly Dictionary<Type, IBundleFactory<Bundle>> factories;
 
-        public DebugBundleContainerBuilder(ICassetteApplication application, Dictionary<Type, IBundleFactory<Bundle>> factories)
+        public BundleContainerFactory(ICassetteApplication application, Dictionary<Type, IBundleFactory<Bundle>> factories)
         {
             this.application = application;
             this.factories = factories;
         }
 
-        public IBundleContainer Build(IEnumerable<Bundle> bundles)
+        public IBundleContainer Create(IEnumerable<Bundle> bundles)
         {
             var externalPaths = new HashSet<string>();
             var externalBundles = new List<Bundle>();
@@ -123,6 +124,107 @@ namespace Cassette.Configuration
                 return factory;
             }
             throw new ArgumentException(string.Format("Cannot find bundle factory for {0}", bundleType.FullName));
+        }
+    }
+
+    public class CachedBundleContainerFactory_Create_Tests
+    {
+        [Fact]
+        public void GivenCacheIsUpToDate_WhenCreateWithBundle_ThenContainerCreatedWithTheGivenBundle()
+        {
+            var bundles = new[] { new Bundle("~/test") };
+            var cache = new Mock<IBundleCache>();
+            cache.Setup(c => c.InitializeBundlesFromCacheIfUpToDate(bundles))
+                 .Returns(true);
+            var application = Mock.Of<ICassetteApplication>();
+
+            var factory = new CachedBundleContainerFactory(cache.Object, application);
+            var container = factory.Create(bundles);
+
+            container.FindBundleContainingPath("~/test").ShouldBeSameAs(bundles[0]);
+        }
+
+        [Fact]
+        public void GivenCacheIsUpToDate_WhenCreateWithBundle_ThenBundleIsNotProcessed()
+        {
+            var bundle = new Mock<Bundle>("~/test");
+            var bundles = new[] { bundle.Object };
+            var cache = new Mock<IBundleCache>();
+            cache.Setup(c => c.InitializeBundlesFromCacheIfUpToDate(bundles))
+                 .Returns(true);
+            var application = Mock.Of<ICassetteApplication>();
+
+            var factory = new CachedBundleContainerFactory(cache.Object, application);
+            factory.Create(bundles);
+
+            bundle.Verify(b => b.Process(application), Times.Never());
+        }
+
+        [Fact]
+        public void GivenCacheOutOfDate_WhenCreateWithBundle_ThenBundleIsProcessed()
+        {
+            var bundle = new Mock<Bundle>("~/test");
+            var bundles = new[] { bundle.Object };
+            var cache = new Mock<IBundleCache>();
+            cache.Setup(c => c.InitializeBundlesFromCacheIfUpToDate(bundles))
+                 .Returns(false);
+            var application = Mock.Of<ICassetteApplication>();
+
+            var factory = new CachedBundleContainerFactory(cache.Object, application);
+            factory.Create(bundles);
+
+            bundle.Verify(b => b.Process(application));
+        }
+
+        [Fact]
+        public void GivenCacheOutOfDate_WhenCreateWithBundle_ThenContainerSavedToCache()
+        {
+            var bundles = new Bundle[0];
+            var cache = new Mock<IBundleCache>();
+            var application = Mock.Of<ICassetteApplication>();
+
+            var factory = new CachedBundleContainerFactory(cache.Object, application);
+            factory.Create(bundles);
+
+            cache.Verify(c => c.SaveBundleContainer(It.IsAny<IBundleContainer>()));
+        }
+    }
+
+    class CachedBundleContainerFactory
+    {
+        readonly IBundleCache cache;
+        readonly ICassetteApplication application;
+
+        public CachedBundleContainerFactory(IBundleCache cache, ICassetteApplication application)
+        {
+            this.cache = cache;
+            this.application = application;
+        }
+
+        public IBundleContainer Create(IEnumerable<Bundle> bundles)
+        {
+            // The bundles may get altered, so force the evaluation of the enumerator first.
+            var bundlesArray = bundles.ToArray();
+
+            if (cache.InitializeBundlesFromCacheIfUpToDate(bundlesArray))
+            {
+                return new BundleContainer(bundlesArray);
+            }
+            else
+            {
+                ProcessAll(bundlesArray);
+                var container = new BundleContainer(bundlesArray);
+                cache.SaveBundleContainer(container);
+                return container;
+            }
+        }
+
+        void ProcessAll(IEnumerable<Bundle> bundlesArray)
+        {
+            foreach (var bundle in bundlesArray)
+            {
+                bundle.Process(application);
+            }
         }
     }
 }
