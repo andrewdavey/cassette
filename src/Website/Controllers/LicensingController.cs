0d1e94a5b7c8c1568e7593081dd2fb8ab290d956
Andrew Davey <andrew@equin.co.uk>
2011-10-25 13:59:19 +0100

    New pricing plan.

--- src/Website/Controllers/LicensingController.cs
+++ src/Website/Controllers/LicensingController.cs
@@ -6,16 +6,6 @@ namespace Website.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Discount = false;
-            ViewBag.Prices = new[] { 199, 399, 599, 799, 999 };
-            ViewBag.PayPalButtonIds = new[]
-            {
-                "MXUPE7ASET542",
-                "7PQ796D57WE34",
-                "EN2MXRGFDY648",
-                "HL5EG6F35JHD2",
-                "AJK6SP49ZZWW8"
-            };
             return View();
         }
 
@@ -26,18 +16,7 @@ namespace Website.Controllers
 
         public ActionResult EpicDiscount()
         {
-            ViewBag.Discount = true;
-            ViewBag.Prices = new[] { 199, 399, 599, 799, 999 };
-            ViewBag.DiscountPrices = new[] { 99, 199, 299, 399, 499 };
-            ViewBag.PayPalButtonIds = new[]
-            {
-                "9YG9Y8XJ7FYJY",
-                "EDBJRHYLZSVUC",
-                "FZFUAP8G6UKHG",
-                "6YY9H8P7UHUCC",
-                "69L8TGD2ELJRN"
-            };
-            return View("Index");
+            return View("ExpiredDiscount");
         }
 
         public ActionResult Purchased()
---
+++ src/Website/assets/scripts/licensing-buy/form.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="~/jquery"/>
+
+$(function () {
+
+    // Dynamically update the total price when the number of servers is changed.
+    var form = $("#buy-form"),
+        serverNumber = form.find("#servernumber"),
+        totalCost = form.find("#totalcost"),
+        licenseHolder = $("#licenseholder");
+
+    serverNumber.change(function () {
+        var value = parseInt(serverNumber.val(), 10);
+        totalCost.text('$' + value);
+    });
+
+    form.submit(function () {
+        if ($.trim(licenseHolder.val()) === "") {
+            alert("Please enter name of company or person to be license-holder.");
+            setTimeout(function () { licenseHolder.focus(); }, 1);
+            return false;
+        }
+    });
+    // For noscript users the form is hidden by default.
+    // So show it now.
+    form.show();
+});
