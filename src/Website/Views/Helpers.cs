a05b6746fa0b4fa525d51173b0643748a2f7ee02
Andrew Davey <andrew@equin.co.uk>
2011-10-25 15:35:59 +0100

    Fix public URLs generated in appharbor.

--- src/Website/Views/Helpers.cs
+++ src/Website/Views/Helpers.cs
@@ -18,6 +18,7 @@ Cassette. If not, see http://www.gnu.org/licenses/.
 */
 #endregion
 
+using System;
 using System.Web.Mvc;
 
 namespace Website.Views
@@ -27,6 +28,26 @@ namespace Website.Views
         public static string DocumentationUrl(this UrlHelper urlHelper, string path)
         {
             return urlHelper.RouteUrl("Documentation", new { path });
+        }
+
+        public static string ToPublicUrl(this UrlHelper urlHelper, string relativeUrl)
+        {
+            var httpContext = urlHelper.RequestContext.HttpContext;
+
+            var uriBuilder = new UriBuilder
+            {
+                Host = httpContext.Request.Url.Host,
+                Path = "/",
+                Port = 80,
+                Scheme = "http",
+            };
+
+            if (httpContext.Request.IsLocal)
+            {
+                uriBuilder.Port = httpContext.Request.Url.Port;
+            }
+
+            return new Uri(uriBuilder.Uri, relativeUrl).AbsoluteUri;
         }
     }
 }
