6e10280c8b97620c631e69b4604340e7ee64737c
Andrew Davey <andrew@equin.co.uk>
2011-11-18 14:00:43 +0000

    Apply license header to files.

--- src/Cassette.Views/Properties/AssemblyInfo.cs
+++ src/Cassette.Views/Properties/AssemblyInfo.cs
@@ -1,4 +1,24 @@
-﻿using System.Reflection;
+﻿#region License
+/*
+Copyright 2011 Andrew Davey
+
+This file is part of Cassette.
+
+Cassette is free software: you can redistribute it and/or modify it under the 
+terms of the GNU General Public License as published by the Free Software 
+Foundation, either version 3 of the License, or (at your option) any later 
+version.
+
+Cassette is distributed in the hope that it will be useful, but WITHOUT ANY 
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+Cassette. If not, see http://www.gnu.org/licenses/.
+*/
+#endregion
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -17,3 +37,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0bfb224f-391f-472c-a628-2b926875f253")]
+
