5017349ca83d6a8a3364a1f50d64989b4af172a0
Andrew Davey <andrew@equin.co.uk>
2011-11-25 23:45:09 +0000

    v0.9.4

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Reflection;
 [assembly: AssemblyProduct("Cassette")]
 [assembly: AssemblyCopyright("Copyright © Andrew Davey 2011")]
 
-[assembly: AssemblyVersion("0.9.3")]
-[assembly: AssemblyFileVersion("0.9.3")]
+[assembly: AssemblyVersion("0.9.4")]
+[assembly: AssemblyFileVersion("0.9.4")]
