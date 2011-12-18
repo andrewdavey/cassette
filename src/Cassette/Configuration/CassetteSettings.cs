0f511435fc833b25679b36cd567008d9dfa592cb
Andrew Davey <andrew@equin.co.uk>
2011-12-18 13:33:06 +0000

    Remove TODO

--- src/Cassette/Configuration/CassetteSettings.cs
+++ src/Cassette/Configuration/CassetteSettings.cs
@@ -74,7 +74,6 @@ namespace Cassette.Configuration
 
         internal IDictionary<Type, IBundleFactory<Bundle>> BundleFactories { get; private set; }
 
-        // TODO: Make this abstract and override in Cassette.Web subclass.
         public IUrlGenerator UrlGenerator { get; set; }
 
         static Dictionary<Type, IBundleFactory<Bundle>> CreateBundleFactories()
