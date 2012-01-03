f4c7bf43b157b626841ffafcd7eeb3fd7aa0db34
Christopher Cliff <ctcliff@gmail.com>
2012-01-03 18:18:58 -0500

    Added hogan files.

---
+++ src/Cassette/HtmlTemplates/HoganTmplPipeline.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Cassette.BundleProcessing;
+using Cassette.Configuration;
+
+namespace Cassette.HtmlTemplates
+{
+    public class HoganTmplPipeline : MutablePipeline<HtmlTemplateBundle>
+    {
+        protected override IEnumerable<IBundleProcessor<HtmlTemplateBundle>> CreatePipeline(HtmlTemplateBundle bundle, CassetteSettings settings)
+        {
+            yield return new AssignHtmlTemplateRenderer(
+                new RemoteHtmlTemplateBundleRenderer(settings.UrlGenerator)
+            );
+            yield return new AssignContentType("text/javascript");
+            if (bundle.IsFromCache) yield break;
+
+            yield return new ParseHtmlTemplateReferences();
+            yield return new CompileHoganTmpl();
+            yield return new RegisterTemplatesWithHoganTmpl(bundle);
+            yield return new ConcatenateAssets();
+        }
+    }
+}
---
+++ src/Cassette/HtmlTemplates/RegisterTemplateWithHoganTmpl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Cassette.Utilities;
+
+namespace Cassette.HtmlTemplates
+{
+    class RegisterTemplateWithHoganTmpl : IAssetTransformer
+    {
+        readonly HtmlTemplateBundle bundle;
+
+        public RegisterTemplateWithHoganTmpl(HtmlTemplateBundle bundle)
+        {
+            this.bundle = bundle;
+        }
+
+        public Func<Stream> Transform(Func<Stream> openSourceStream, IAsset asset)
+        {
+            return delegate
+            {
+                var id = bundle.GetTemplateId(asset);
+                var compiled = openSourceStream().ReadToEnd();
+                var ns = "JST";
+                var template = ns + "['" + id + "']";
+                var sb = new StringBuilder();
+                
+                sb.AppendLine(";(function(){");
+                sb.AppendLine("window." + ns + "=window." + ns + "||{};");
+                sb.AppendLine(template + "= new HoganTemplate();");
+                sb.AppendLine(template + ".render = " + compiled);
+                sb.AppendLine("})();");
+                
+                return sb.ToString().AsStream();
+            };
+        }
+    }
+}
