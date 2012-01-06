5ee0304423e1dd2c9c41167c77e6f6d4b88d7e42
Andrew Davey <andrew@equin.co.uk>
2012-01-06 11:01:53 +0000

    Create PreBuiltCassetteApplication class to be used when bundles were built and compile-time

---
+++ src/Cassette.Web/PreBuiltCassetteApplication.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web;
+using Cassette.Configuration;
+
+namespace Cassette
+{
+    class PreBuiltCassetteApplication : ICassetteApplication
+    {
+        readonly Func<HttpContextBase> getCurrentHttpContext;
+
+        public PreBuiltCassetteApplication(string preBuiltBundlesManifestFilename, Func<HttpContextBase> getCurrentHttpContext)
+        {
+            this.getCurrentHttpContext = getCurrentHttpContext;
+        }
+
+        public CassetteSettings Settings
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public T FindBundleContainingPath<T>(string path) where T : Bundle
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReferenceBuilder GetReferenceBuilder()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void Dispose()
+        {
+            
+        }
+    }
+}
