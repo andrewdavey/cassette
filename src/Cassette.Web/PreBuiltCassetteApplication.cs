d88b22ae936b24992f00ed10219ef1e87733200d
Andrew Davey <andrew@equin.co.uk>
2012-01-08 11:08:50 +0000

    Add BundlesArePreBuilt setting

--- src/Cassette.Web/PreBuiltCassetteApplication.cs
+++ src/Cassette.Web/PreBuiltCassetteApplication.cs
@@ -2,20 +2,25 @@
 using System.Web;
 using Cassette.Configuration;
 
-namespace Cassette
+namespace Cassette.Web
 {
     class PreBuiltCassetteApplication : ICassetteApplication
     {
         readonly Func<HttpContextBase> getCurrentHttpContext;
+        readonly CassetteSettings settings;
 
-        public PreBuiltCassetteApplication(string preBuiltBundlesManifestFilename, Func<HttpContextBase> getCurrentHttpContext)
+        public PreBuiltCassetteApplication(string appBundlesPath, Func<HttpContextBase> getCurrentHttpContext)
         {
             this.getCurrentHttpContext = getCurrentHttpContext;
+            settings = new CassetteSettings("")
+            {
+                BundlesArePreBuilt = true
+            };
         }
 
         public CassetteSettings Settings
         {
-            get { throw new NotImplementedException(); }
+            get { return settings; }
         }
 
         public T FindBundleContainingPath<T>(string path) where T : Bundle
@@ -25,9 +30,26 @@ namespace Cassette
 
         public IReferenceBuilder GetReferenceBuilder()
         {
-            throw new NotImplementedException();
+            return GetOrCreateReferenceBuilder();
         }
-        
+
+        IReferenceBuilder GetOrCreateReferenceBuilder()
+        {
+            const string key = "Cassette.ReferenceBuilder";
+            var items = getCurrentHttpContext().Items;
+            if (items.Contains(key))
+            {
+                return (IReferenceBuilder)items[key];
+            }
+            else
+            {
+                var builder = new ReferenceBuilder(bundleContainer, settings.BundleFactories, new PlaceholderTracker(),
+                                                   Settings);
+                items[key] = builder;
+                return builder;
+            }
+        }
+
         public void Dispose()
         {
             
