6dd6c11e875828d716fc548d7e66128ff86e1291
Andrew Davey <andrew@equin.co.uk>
2012-01-30 12:32:56 +0000

    Create the AssignHash bundle processor.

---
+++ src/Cassette.UnitTests/BundleProcessing/AssignHash.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using Cassette.Configuration;
+using Cassette.Utilities;
+using Moq;
+using Should;
+using Xunit;
+
+namespace Cassette.BundleProcessing
+{
+    public class AssignHash_Tests : IDisposable
+    {
+        readonly AssignHash assignHash;
+        readonly TestableBundle bundle;
+        readonly SHA1 sha1;
+
+        public AssignHash_Tests()
+        {
+            assignHash = new AssignHash();
+            bundle = new TestableBundle("~");
+            sha1 = SHA1.Create();
+        }
+
+        [Fact]
+        public void GivenBundleHasNoAssetsThenBundleHashIsSHA1OfEmptyStream()
+        {
+            ProcessBundleWithAssignHash();
+            AssertHashIsSha1Of("");
+        }
+
+        [Fact]
+        public void GivenBundleHasOneAssetThenBundleHashIsSHA1OfAssetContent()
+        {
+            var asset = StubAsset("content");
+            bundle.Assets.Add(asset);
+
+            ProcessBundleWithAssignHash();
+
+            AssertHashIsSha1Of("content");
+        }
+
+        [Fact]
+        public void GivenBundleHasOneAssetThatContainsTwoAssetsThenBundleHashIsSHA1OfTheTwoChildAssets()
+        {
+            var childAsset1 = StubAsset("asset-1");
+            var childAsset2 = StubAsset("asset-2");
+            var combinedAsset = CombinedAsset(childAsset1, childAsset2);
+            bundle.Assets.Add(combinedAsset);
+
+            ProcessBundleWithAssignHash();
+
+            AssertHashIsSha1Of("asset-1asset-2");
+        }
+
+        IAsset StubAsset(string content)
+        {
+            var asset = new Mock<IAsset>();
+            asset.Setup(a => a.OpenStream()).Returns(() => content.AsStream());
+            asset.Setup(a => a.Accept(It.IsAny<IBundleVisitor>()))
+                 .Callback<IBundleVisitor>(v => v.Visit(asset.Object));
+            return asset.Object;
+        }
+
+        IAsset CombinedAsset(IAsset childAsset1, IAsset childAsset2)
+        {
+            var combinedAsset = new Mock<IAsset>();
+            combinedAsset.Setup(a => a.Accept(It.IsAny<IBundleVisitor>()))
+                .Callback<IBundleVisitor>(v =>
+                {
+                    childAsset1.Accept(v);
+                    childAsset2.Accept(v);
+                });
+            return combinedAsset.Object;
+        }
+
+        void ProcessBundleWithAssignHash()
+        {
+            assignHash.Process(bundle, new CassetteSettings(""));
+        }
+
+        void AssertHashIsSha1Of(string expected)
+        {
+            using (var stream = expected.AsStream())
+            {
+                bundle.Hash.ShouldEqual(sha1.ComputeHash(stream));
+            }
+        }
+
+        void IDisposable.Dispose()
+        {
+           sha1.Dispose();
+        }
+    }
+
+    public class AssignHash : IBundleProcessor<Bundle>
+    {
+        public void Process(Bundle bundle, CassetteSettings settings)
+        {
+            using (var concatenatedStream = new MemoryStream())
+            {
+                bundle.Accept(new ConcatenatedStreamBuilder(concatenatedStream));
+
+                concatenatedStream.Position = 0;
+                bundle.Hash = ComputeSha1Hash(concatenatedStream);
+            }
+        }
+
+        byte[] ComputeSha1Hash(Stream concatenatedStream)
+        {
+            using (var sha1 = SHA1.Create())
+            {
+                return sha1.ComputeHash(concatenatedStream);
+            }
+        }
+
+        class ConcatenatedStreamBuilder : IBundleVisitor
+        {
+            readonly Stream concatenatedStream;
+
+            public ConcatenatedStreamBuilder(Stream concatenatedStream)
+            {
+                this.concatenatedStream = concatenatedStream;
+            }
+
+            public void Visit(Bundle bundle)
+            {
+            }
+
+            public void Visit(IAsset asset)
+            {
+                using (var stream = asset.OpenStream())
+                {
+                    stream.CopyTo(concatenatedStream);
+                }
+            }
+        }
+    }
+}
