7dab33c3343cf4c0e95163dbce282adc964dafb2
Andrew Davey <andrew@equin.co.uk>
2012-01-31 21:36:38 +0000

    Add CachedBundleContainerFactory test.

--- src/Cassette.UnitTests/Configuration/CachedBundleContainerFactory.cs
+++ src/Cassette.UnitTests/Configuration/CachedBundleContainerFactory.cs
@@ -1,5 +1,10 @@
-﻿using Cassette.Manifests;
+﻿using System.Linq;
+using Cassette.Manifests;
+using Cassette.Scripts;
+using Cassette.Scripts.Manifests;
 using Moq;
+using Should;
+using Xunit;
 
 namespace Cassette.Configuration
 {
@@ -11,6 +16,27 @@ namespace Cassette.Configuration
         {
             cache = new Mock<ICassetteManifestCache>();
             cache.Setup(c => c.LoadCassetteManifest()).Returns(() => new CassetteManifest());
+        }
+
+        [Fact]
+        public void GivenCachedManifestWithOneBundle_WhenCreateWithMatchingUpToDateBundle_ThenCachedBundleIsReturned()
+        {
+            var cachedManifest = new CassetteManifest(
+                "",
+                new[]
+                {
+                    new ScriptBundleManifest { Path = "~", Hash = new byte[] { 1, 2, 3 } }
+                }
+            );
+            cache.Setup(c => c.LoadCassetteManifest()).Returns(cachedManifest);
+
+            var factory = CreateFactory();
+            var container = factory.Create(new[] { new ScriptBundle("~") });
+
+            var bundle = container.Bundles.Single();
+            bundle.Hash.ShouldEqual(new byte[] { 1, 2, 3 });
+            bundle.IsFromCache.ShouldBeTrue();
+            bundle.IsProcessed.ShouldBeTrue();
         }
 
         internal override IBundleContainerFactory CreateFactory()
