dda069eb82e4c298ed75ecc2a99f8cfc25ab7490
Andrew Davey <andrew@equin.co.uk>
2012-02-06 18:41:28 +0000

    Add samples directory and create Precompiled sample project.

---
+++ samples/Precompiled/Precompiled/CassetteConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Cassette.Configuration;
+using Cassette.Scripts;
+using Cassette.Stylesheets;
+
+namespace Precompiled
+{
+    public class CassetteConfiguration : ICassetteConfiguration
+    {
+        public void Configure(BundleCollection bundles, CassetteSettings settings)
+        {
+            bundles.Add<StylesheetBundle>("Content");
+            bundles.Add<ScriptBundle>("Scripts");
+        }
+    }
+}
---
+++ samples/Precompiled/Precompiled/Scripts/home.index.js
@@ -0,0 +1,1 @@
+﻿/* File Created: February 6, 2012 */
