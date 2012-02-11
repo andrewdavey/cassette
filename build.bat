8c75f38b14bf8ffa62d578a2548596a4a598fa1d
Andrew Davey <andrew@equin.co.uk>
2012-02-11 11:43:44 +0000

    Upgrade to xunit.net 1.9. Also use single build.bat to build, test and nuget-pack.

--- build.bat
+++ build.bat
@@ -7,9 +7,21 @@ if exist build\bin. (
 )
 
 %msbuild% src\Cassette.UnitTests\Cassette.UnitTests.csproj /p:Configuration=Release /p:OutDir=..\..\build\bin\
-
 %msbuild% src\Cassette.IntegrationTests\Cassette.IntegrationTests.csproj /p:Configuration=Release /p:OutDir=..\..\build\bin\
-
 %msbuild% src\Cassette.Web\Cassette.Web.csproj /p:Configuration=Release /p:OutDir=..\..\build\bin\
-
 %msbuild% src\Cassette.Views\Cassette.Views.csproj /p:Configuration=Release /p:OutDir=..\..\build\bin\
+
+
+
+tools\xunit.console.clr4.x86.exe build\bin\Cassette.UnitTests.dll /noshadow
+tools\xunit.console.clr4.x86.exe build\bin\Cassette.IntegrationTests.dll /noshadow
+
+if exist build\nuget. (
+	del /S /Q build\nuget 
+) else (
+	mkdir build\nuget
+)
+
+nuget pack -Build src\Cassette\Cassette.csproj -Symbols -OutputDirectory build\nuget -Prop Configuration=Release
+nuget pack -Build src\Cassette.Views\Cassette.Views.csproj -Symbols -OutputDirectory build\nuget -Prop Configuration=Release
+nuget pack -Build src\Cassette.Web\Cassette.Web.csproj -Symbols -OutputDirectory build\nuget -Prop Configuration=Release
