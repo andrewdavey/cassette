b87e747701fa56ba29a27e653572a728fbfea97e
Andrew Davey <andrew@equin.co.uk>
2012-02-12 16:38:26 +0000

    Create new msbuild-based build script.

--- build.bat
+++ build.bat
@@ -1,27 +1,7 @@
 set msbuild=%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
 
-if exist build\bin. (
-	del /S /Q build\bin\*
+if "%1" == "" (
+	%msbuild% build.xml
 ) else (
-	mkdir build\bin
+	%msbuild% build.xml /target:%1
 )
-
-%msbuild% src\Cassette.UnitTests\Cassette.UnitTests.csproj /p:Configuration=Release /p:OutDir=..\..\build\bin\
-%msbuild% src\Cassette.IntegrationTests\Cassette.IntegrationTests.csproj /p:Configuration=Release /p:OutDir=..\..\build\bin\
-%msbuild% src\Cassette.Web\Cassette.Web.csproj /p:Configuration=Release /p:OutDir=..\..\build\bin\
-%msbuild% src\Cassette.Views\Cassette.Views.csproj /p:Configuration=Release /p:OutDir=..\..\build\bin\
-
-
-
-tools\xunit.console.clr4.x86.exe build\bin\Cassette.UnitTests.dll /noshadow
-tools\xunit.console.clr4.x86.exe build\bin\Cassette.IntegrationTests.dll /noshadow
-
-if exist build\nuget. (
-	del /S /Q build\nuget 
-) else (
-	mkdir build\nuget
-)
-
-nuget pack -Build src\Cassette\Cassette.csproj -Symbols -OutputDirectory build\nuget -Prop Configuration=Release
-nuget pack -Build src\Cassette.Views\Cassette.Views.csproj -Symbols -OutputDirectory build\nuget -Prop Configuration=Release
-nuget pack -Build src\Cassette.Web\Cassette.Web.csproj -Symbols -OutputDirectory build\nuget -Prop Configuration=Release
