6ec9e7e27bf1764429f5d143b280d185c59d49a8
Andrew Davey <andrew@equin.co.uk>
2012-02-19 12:55:12 +0000

    Prevent multiple enumerable evaluation.

--- src/Cassette/Configuration/BundleCollectionExtensions.cs
+++ src/Cassette/Configuration/BundleCollectionExtensions.cs
@@ -274,19 +274,18 @@ namespace Cassette.Configuration
             foreach (var directory in directories)
             {
                 Trace.Source.TraceInformation(string.Format("Creating {0} for {1}", typeof(T).Name, directory.FullPath));
-                var allFiles = fileSearch.FindFiles(directory);
-                if (allFiles.Any())
-                {
-                    var descriptorFile = TryGetDescriptorFile(directory);
-                    var descriptor = descriptorFile.Exists
-                                         ? new BundleDescriptorReader(descriptorFile).Read()
-                                         : new BundleDescriptor { AssetFilenames = { "*" } };
+                var allFiles = fileSearch.FindFiles(directory).ToArray();
+                if (!allFiles.Any()) continue;
 
-                    var bundle = bundleFactory.CreateBundle(directory.FullPath, allFiles, descriptor);
-                    if (customizeBundle != null) customizeBundle(bundle);
-                    TraceAssetFilePaths(bundle);
-                    bundleCollection.Add(bundle);
-                }
+                var descriptorFile = TryGetDescriptorFile(directory);
+                var descriptor = descriptorFile.Exists
+                                     ? new BundleDescriptorReader(descriptorFile).Read()
+                                     : new BundleDescriptor { AssetFilenames = { "*" } };
+
+                var bundle = bundleFactory.CreateBundle(directory.FullPath, allFiles, descriptor);
+                if (customizeBundle != null) customizeBundle(bundle);
+                TraceAssetFilePaths(bundle);
+                bundleCollection.Add(bundle);
             }
         }
 
