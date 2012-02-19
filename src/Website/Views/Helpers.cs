e0136642ef147c66dcd85ed83b68aa655e359172
Andrew Davey <andrew@equin.co.uk>
2012-02-19 17:33:59 +0000

    Remove unused method

--- src/Website/Views/Helpers.cs
+++ src/Website/Views/Helpers.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Website.Views
 {
@@ -8,26 +7,6 @@ namespace Website.Views
         public static string DocumentationUrl(this UrlHelper urlHelper, string path)
         {
             return urlHelper.RouteUrl("Documentation", new { path });
-        }
-
-        public static string ToPublicUrl(this UrlHelper urlHelper, string relativeUrl)
-        {
-            var httpContext = urlHelper.RequestContext.HttpContext;
-
-            var uriBuilder = new UriBuilder
-            {
-                Host = httpContext.Request.Url.Host,
-                Path = "/",
-                Port = 80,
-                Scheme = "http",
-            };
-
-            if (httpContext.Request.IsLocal)
-            {
-                uriBuilder.Port = httpContext.Request.Url.Port;
-            }
-
-            return new Uri(uriBuilder.Uri, relativeUrl).AbsoluteUri;
         }
     }
 }
