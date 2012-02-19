21b85b45b7357d7efa2eaaae5fe84a1aa301e1a6
Andrew Davey <andrew@equin.co.uk>
2012-02-19 18:04:25 +0000

    Fixed mistake in nuspec that was not including Cassette.dll. Not before I'd push beta3 to nuget.org though, so had to bump up to beta4

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -5,7 +5,7 @@
 [assembly: AssemblyCopyright("Copyright © 2011 Andrew Davey")]
 
 // NOTE: When changing this version, also update Cassette.MSBuild\Cassette.targets to match.
-[assembly: AssemblyInformationalVersion("1.1.0-beta3")]
+[assembly: AssemblyInformationalVersion("1.1.0-beta4")]
 
 [assembly: AssemblyVersion("1.1.0.*")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
