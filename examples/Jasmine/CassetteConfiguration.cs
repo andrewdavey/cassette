153d5d45179ef2dce760ec46bc5858599239ab38
Andrew Davey <andrew@equin.co.uk>
2012-02-20 18:35:53 +0000

    Add Jasmine example project

---
+++ examples/Jasmine/CassetteConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Cassette.Configuration;
+using Cassette.Scripts;
+
+namespace Jasmine
+{
+    public class CassetteConfiguration : ICassetteConfiguration
+    {
+        public void Configure(BundleCollection bundles, CassetteSettings settings)
+        {
+            // The "Scripts/app" contains the application script we'll be testing with Jasmine.
+            // The "Scripts/specs" contains the Jasmine specs. It is treated just like a regular Cassette bundle.
+            // The "Scripts/jasmine" is usually provided by the Cassette.Web.Jasmine nuget package.
+
+            bundles.Add<ScriptBundle>("Scripts/app");
+            bundles.Add<ScriptBundle>("Scripts/specs");
+        }
+    }
+}
