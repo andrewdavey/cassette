e3ecb484064088b38f42b0d246650df69b03e93e
Andrew Davey <andrew@equin.co.uk>
2012-02-20 18:35:53 +0000

    Add Jasmine example project

---
+++ examples/Jasmine/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Jasmine
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
