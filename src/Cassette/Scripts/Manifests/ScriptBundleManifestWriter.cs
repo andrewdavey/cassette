9b1f6843dc151aff79203e43028d59de1ade748a
James Simm <james.simm@gmail.com>
2012-02-22 08:03:45 +0000

    Passing Tests on Manifest Reader/Writer

--- src/Cassette/Scripts/Manifests/ScriptBundleManifestWriter.cs
+++ src/Cassette/Scripts/Manifests/ScriptBundleManifestWriter.cs
@@ -3,10 +3,43 @@ using Cassette.Manifests;
 
 namespace Cassette.Scripts.Manifests
 {
+    class ScriptBundleManifestWriter<T> : BundleManifestWriter<T>
+        where T : ScriptBundleManifest
+    {
+        protected ScriptBundleManifestWriter(XContainer container)
+            : base(container)
+        {
+        }
+
+        protected override XElement CreateElement()
+        {
+            var element = base.CreateElement();
+            if (Manifest.Condition != null)
+            {
+                element.Add(new XAttribute("Condition", Manifest.Condition));
+            }
+
+            return element;
+        }
+    }
+
     class ScriptBundleManifestWriter : BundleManifestWriter<ScriptBundleManifest>
     {
-        public ScriptBundleManifestWriter(XContainer container) : base(container)
+        public ScriptBundleManifestWriter(XContainer container) 
+            : base(container)
         {
+        }
+
+        protected override XElement CreateElement()
+        {
+            var element = base.CreateElement();
+
+            if (Manifest.Condition != null)
+            {
+                element.Add(new XAttribute("Condition", Manifest.Condition));
+            }
+
+            return element;
         }
     }
 }
