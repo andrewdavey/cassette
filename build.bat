6d4f929a5e5c0185f3b6d85565d66ead2eec5353
Kamran Ayub <kamran.ayub@gmail.com>
2012-02-22 16:31:01 -0600

    Updated Nuget packing
    Changed how Nuget packaging works to support multiple framework targeting. Updated Fx35 docs with a gotcha I ran into during testing.

--- build.bat
+++ build.bat
@@ -2,8 +2,6 @@ set msbuild=%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
 
 if "%1" == "" (
 	%msbuild% build.xml
-  %msbuild% build.xml /p:Framework="v3.5"
 ) else (
 	%msbuild% build.xml /target:%1
-  %msbuild% build.xml /target:%1 /p:Framework="v3.5"
 )
