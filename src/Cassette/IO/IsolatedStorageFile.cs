b75828d65745458c0296fb1158e1de4b04fd1512
Kamran Ayub <kamran.ayub@gmail.com>
2012-02-23 08:39:29 -0600

    Made build test both lib40 and lib35 assemblies; removed index file workaround in IsolatedStorageFile and opted to throw NotImplementedException instead for LastWriteTime

--- src/Cassette/IO/IsolatedStorageFile.cs
+++ src/Cassette/IO/IsolatedStorageFile.cs
@@ -13,9 +13,6 @@ namespace Cassette.IO
         readonly Func<Storage> getStorage;
         readonly IsolatedStorageDirectory directory;
         readonly string systemFilename;
-#if NET35
-        const string IndexFilename = "index.cassette";
-#endif
 
         public IsolatedStorageFile(string filename, Storage storage, IsolatedStorageDirectory directory)
             : this(filename, () => storage, directory)
@@ -28,10 +25,6 @@ namespace Cassette.IO
             this.getStorage = getStorage;
             this.directory = directory;
             systemFilename = filename.Substring(2); // Skip the "~/" prefix.
-#if NET35
-            // Build index for this file, if it doesn't already exist
-            WriteLastWriteTimeUtc();
-#endif
         }
 
         public IDirectory Directory
@@ -47,11 +40,6 @@ namespace Cassette.IO
         public Stream Open(FileMode mode, FileAccess access, FileShare fileShare)
         {
 #if NET35
-            // Use index file to store write times
-            if (access == FileAccess.Write)
-            {
-                WriteLastWriteTimeUtc();
-            }
             return new IsolatedStorageFileStream(systemFilename, mode, access, fileShare, Storage);
 #endif
 #if NET40
@@ -77,7 +65,7 @@ namespace Cassette.IO
             get
             {
 #if NET35
-                return ReadLastWriteTimeUtc();
+                throw new NotImplementedException();
 #endif
 #if NET40
                 return Storage.GetLastWriteTime(systemFilename).UtcDateTime;
@@ -87,10 +75,6 @@ namespace Cassette.IO
 
         public void Delete()
         {
-#if NET35
-            // Remove from index
-            RemoveFromIndex();
-#endif
             Storage.DeleteFile(systemFilename);
         }
 
@@ -98,107 +82,5 @@ namespace Cassette.IO
         {
             get { return getStorage(); }
         }
-
-#if NET35
-        private DateTime ReadLastWriteTimeUtc()
-        {
-            long writeTime;
-            if (GetIndex().TryGetValue(systemFilename, out writeTime))
-            {
-                return DateTime.FromFileTimeUtc(writeTime);
-            }
-            else
-            {
-                throw new IOException("Could not find last write time for " + systemFilename);
-            }
-        }
-
-        private FileIndex GetIndex()
-        {
-            FileIndex index;
-
-            using (var reader = new StreamReader(OpenIndexFile(FileMode.OpenOrCreate, FileAccess.Read)))
-            {
-                index = new FileIndex(reader.ReadToEnd());
-            }
-
-            return index;
-        }
-
-        private void WriteLastWriteTimeUtc()
-        {
-            var index = GetIndex();
-
-            // Open writing
-            using (var writer = new StreamWriter(OpenIndexFile(FileMode.OpenOrCreate, FileAccess.ReadWrite)))
-            {
-                if (index.ContainsKey(systemFilename))
-                {
-                    // Update existing
-                    index[systemFilename] = DateTime.UtcNow.ToFileTimeUtc();
-                }
-                else
-                {
-                    // Append
-                    index.Add(systemFilename, DateTime.UtcNow.ToFileTimeUtc());
-                }
-
-                writer.Write(index.ToString());
-            }
-        }
-
-        private void RemoveFromIndex()
-        {
-            var index = GetIndex();
-
-            if (index.ContainsKey(systemFilename))
-            {
-                index.Remove(systemFilename);
-
-                using (var writer = new StreamWriter(OpenIndexFile(FileMode.Open, FileAccess.Write)))
-                {
-                    writer.Write(index.ToString());
-                }
-            }
-        }
-
-        private Stream OpenIndexFile(FileMode mode, FileAccess access)
-        {
-            return new IsolatedStorageFileStream(IndexFilename, mode, access, Storage);
-        }
-
-        private class FileIndex : Dictionary<string, long>
-        {
-
-            /// <summary>
-            /// Reads in a text file contents and parses into dictionary
-            /// </summary>
-            /// <param name="textFile"></param>
-            public FileIndex(string textFile)
-            {
-                var lines = textFile.Split(new []{'\n'}, StringSplitOptions.RemoveEmptyEntries);
-
-                foreach (var line in lines)
-                {
-                    var pair = line.Split('|');
-
-                    if (pair.Length == 2)
-                    {
-                        this.Add(pair[0], Convert.ToInt64(pair[1]));
-                    }                    
-                }
-            }
-
-            /// <summary>
-            /// Writes index to a string for serialization
-            /// </summary>
-            /// <returns></returns>
-            public override string ToString()
-            {
-                return String.Join("\n", this.Select(kv => kv.Key + "|" + kv.Value).ToArray());
-            }
-        }
-
-#endif
     }
 }
