57858f7fc16bec1c94dac664720653c7898c34a0
Kamran Ayub <kamran.ayub@gmail.com>
2012-02-23 15:33:07 -0600

    Instead of commenting out, wrapped net40 specific test within pragma

--- src/Cassette.UnitTests/IO/IsolatedStorageFileWrapper.cs
+++ src/Cassette.UnitTests/IO/IsolatedStorageFileWrapper.cs
@@ -57,19 +57,16 @@ namespace Cassette.IO
 
         // According to andrew, LastWriteTime is actually never really called in
         // code, so we should be able to safely ignore it (for FX35)
-        //[Fact]
-        //public void GetLastWriteTimeUtcReturnsFileWriteTime()
-        //{
-        //    var file = new IsolatedStorageFile("~/exists.js", storage, directory);
+#if NET40
+        [Fact]
+        public void GetLastWriteTimeUtcReturnsFileWriteTime()
+        {
+            var file = new IsolatedStorageFile("~/exists.js", storage, directory);
+            var low = storage.GetLastWriteTime("exists.js").UtcDateTime;
 
-        //    // FX35 workaround: range of accepted values
-        //    var low = storage.GetLastWriteTime("exists.js").UtcDateTime;
-        //    var high = low.AddMilliseconds(500); // threshold, 500ms
-
-        //    file.LastWriteTimeUtc.ShouldBeInRange(low, high);
-        //    // FX40 file.LastWriteTimeUtc.ShouldEqual(low);
-        //}
-
+            file.LastWriteTimeUtc.ShouldEqual(low);
+        }
+#endif
         [Fact]
         public void OpenStreamReturnsFileStream()
         {
