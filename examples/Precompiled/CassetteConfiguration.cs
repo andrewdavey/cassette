49556afca2a432689b1e6f1354e99923da50c51b
Andrew Davey <andrew@equin.co.uk>
2012-03-14 18:28:07 +0000

    Update Precompiled example project.

--- examples/Precompiled/CassetteConfiguration.cs
+++ examples/Precompiled/CassetteConfiguration.cs
@@ -8,6 +8,12 @@ namespace Precompiled
     {
         public void Configure(BundleCollection bundles, CassetteSettings settings)
         {
+            // This configuration is first used at compile time to generate a manifest of all bundles.
+
+            // Then at runtime it is called again, but this time settings.IsUsingPrecompiledManifest == true.
+            // So we can skip adding the bundles. They will be added automatically from the manifest.
+            if (settings.IsUsingPrecompiledManifest) return;
+
             bundles.Add<StylesheetBundle>("Content");
             bundles.Add<ScriptBundle>("Scripts");
         }
