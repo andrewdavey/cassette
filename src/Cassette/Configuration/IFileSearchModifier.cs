4bcab1d57fe2803e95bf366f5488a311eef16435
Andrew Davey <andrew@equin.co.uk>
2012-04-20 12:28:03 +0100

    Clarify the apparently unused generic type parameter.

--- src/Cassette/Configuration/IFileSearchModifier.cs
+++ src/Cassette/Configuration/IFileSearchModifier.cs
@@ -1,6 +1,10 @@
 namespace Cassette.Configuration
 {
+// ReSharper disable UnusedTypeParameter
+// The T type parameter makes it easy to distinguish between file search modifiers for the different type of bundles
     public interface IFileSearchModifier<T>
+        where T : Bundle
+// ReSharper restore UnusedTypeParameter
     {
         void Modify(FileSearch fileSearch);
     }
