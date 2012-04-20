9fe8658e72d48a1accacfb6d368ddfb49aa0dc5d
Andrew Davey <andrew@equin.co.uk>
2012-04-20 12:59:52 +0100

    Start implementing FileAccessPermission

---
+++ src/Cassette.UnitTests/FileAccessPermission.cs
@@ -0,0 +1,35 @@
+﻿using Should;
+using Xunit;
+namespace Cassette
+{
+    public class FileAccessPermission_Tests
+    {
+        readonly FileAccessPermission permission = new FileAccessPermission();
+
+        [Fact]
+        public void GivenAllowAccessToPath_ThenCanAccessPathReturnsTrue()
+        {
+            permission.AllowAccess("~/test.png");
+            permission.CanAccess("~/test.png").ShouldBeTrue();
+        }
+
+        [Fact]
+        public void GivenNoPathsAllowed_ThenCanAccessPathReturnsFalse()
+        {
+            permission.CanAccess("~/test.png").ShouldBeFalse();
+        }
+    }
+
+    public class FileAccessPermission
+    {
+        public void AllowAccess(string path)
+        {
+            
+        }
+
+        public bool CanAccess(string path)
+        {
+            return true;
+        }
+    }
+}
