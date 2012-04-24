b6bc268cb2ba55af7ffc61489a45b31e675e5ced
Kobi <kobikobi@gmail.com>
2012-04-24 10:44:37 +0300

    Allow inline <script> tags and don't wrap them in another script tag - allows syntax highlighting and intellisense on AddInlineScript: Bundles.AddInlineScript(@<script type="text/javascript"> var i = 1;</script>);

--- src/Cassette/Scripts/InlineScriptBundle.cs
+++ src/Cassette/Scripts/InlineScriptBundle.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
+using System.Text.RegularExpressions;
 using Cassette.Configuration;
 
 namespace Cassette.Scripts
 {
-    class InlineScriptBundle : ScriptBundle
+    internal class InlineScriptBundle : ScriptBundle
     {
         readonly string scriptContent;
 
@@ -16,15 +17,34 @@ namespace Cassette.Scripts
         {
         }
 
-        internal override string Render()
+        static readonly Regex DetectScriptRegex = new Regex(@"\A \s* <script \b",
+                                                            RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace);
+        /// <summary>
+        /// Handle cases of the content already wrapped in a &lt;script&gt; tag.
+        /// </summary>
+        /// <returns></returns>
+        protected string GetScriptContent()
         {
-            var content = string.Format(
+            var isContentScriptTag = scriptContent != null &&
+                                     DetectScriptRegex.IsMatch(scriptContent, 0);
+            var htmlAttributes = HtmlAttributes.CombinedAttributes; // should start with a space
+
+            if (isContentScriptTag)
+            {
+                return DetectScriptRegex.Replace(scriptContent,
+                    m => m.Value + htmlAttributes, 1, 0); // don't need a space after the attributes - the regex is checking for "\b"
+            }
+            return String.Format(
                 HtmlConstants.InlineScriptHtml,
-                HtmlAttributes.CombinedAttributes,
+                htmlAttributes,
                 Environment.NewLine,
                 scriptContent
-            );
+                );
+        }
 
+        internal override string Render()
+        {
+            var content = GetScriptContent();
             var conditionalRenderer = new ConditionalRenderer();
             return conditionalRenderer.Render(Condition, html => html.Append(content));
         }
