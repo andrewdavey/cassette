ef353c1298e6e97326c3e74b5d67620baa88e95a
Andrew Davey <andrew@equin.co.uk>
2012-06-27 07:55:50 +0100

    Add more diagnostic handler tests - script, stylesheet and html template JSON data

--- src/Cassette.Aspnet.UnitTests/DiagnosticRequestHandler.cs
+++ src/Cassette.Aspnet.UnitTests/DiagnosticRequestHandler.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections.Specialized;
+using System.Linq;
 using System.Web;
 using System.Web.Routing;
+using Cassette.HtmlTemplates;
 using Cassette.Scripts;
+using Cassette.Stylesheets;
 using Cassette.Views;
 using Moq;
+using Should;
 using Xunit;
 
 namespace Cassette.Aspnet
@@ -15,6 +19,8 @@ namespace Cassette.Aspnet
         readonly CassetteSettings settings;
         readonly Mock<HttpResponseBase> response;
         readonly Mock<IBundleCacheRebuilder> rebuilder;
+        readonly BundleCollection bundles;
+        readonly Mock<IBundlesHelper> helper;
 
         public DiagnosticRequestHandler_Tests()
         {
@@ -23,10 +29,10 @@ namespace Cassette.Aspnet
             httpContext.SetupGet(c => c.Response).Returns(response.Object);
             var requestContext = new RequestContext(httpContext.Object, new RouteData());
             settings = new CassetteSettings();
-            var bundles = new BundleCollection(settings, Mock.Of<IFileSearchProvider>(), Mock.Of<IBundleFactoryProvider>());
+            bundles = new BundleCollection(settings, Mock.Of<IFileSearchProvider>(), Mock.Of<IBundleFactoryProvider>());
             var urlGenerator = Mock.Of<IUrlGenerator>();
             rebuilder = new Mock<IBundleCacheRebuilder>();
-            var helper = new Mock<IBundlesHelper>();
+            helper = new Mock<IBundlesHelper>();
             helper.Setup(h => h.Render<ScriptBundle>(null)).Returns(new HtmlString(""));
             Bundles.Helper = helper.Object;
             httpContext.SetupGet(c => c.Request.HttpMethod).Returns("GET");
@@ -52,6 +58,54 @@ namespace Cassette.Aspnet
             handler.ProcessRequest();
 
             response.Verify(r => r.Write(It.Is<string>(html => html.StartsWith("<!DOCTYPE html>"))));
+        }
+
+        [Fact]
+        public void GivenScriptBundleInCollection_WhenProcessRequest_ThenPageDataScriptIncludesBundleJson()
+        {
+            GivenLocalRequest();
+            bundles.Add(new ScriptBundle("~/test"));
+            PageDataScriptBundle bundle = null;
+            helper
+                .Setup(h => h.Reference(It.IsAny<PageDataScriptBundle>(), null))
+                .Callback<Bundle, string>((referenced, _) => bundle = (PageDataScriptBundle)referenced);
+
+            handler.ProcessRequest();
+
+            var script = bundle.Render();
+            script.ShouldContain("Scripts=[{\"Path\":\"~/test\",\"Url\":null,\"Condition\":null,\"Assets\":[],\"References\":[],\"Size\":-1}];");
+        }
+
+        [Fact]
+        public void GivenStylesheetBundleInCollection_WhenProcessRequest_ThenPageDataScriptIncludesBundleJson()
+        {
+            GivenLocalRequest();
+            bundles.Add(new StylesheetBundle("~/test"));
+            PageDataScriptBundle bundle = null;
+            helper
+                .Setup(h => h.Reference(It.IsAny<PageDataScriptBundle>(), null))
+                .Callback<Bundle, string>((referenced, _) => bundle = (PageDataScriptBundle)referenced);
+
+            handler.ProcessRequest();
+
+            var script = bundle.Render();
+            script.ShouldContain("Stylesheets=[{\"Path\":\"~/test\",\"Url\":null,\"Media\":null,\"Condition\":null,\"Assets\":[],\"References\":[],\"Size\":-1}];");
+        }
+
+        [Fact]
+        public void GivenHtmlTemplateBundleInCollection_WhenProcessRequest_ThenPageDataScriptIncludesBundleJson()
+        {
+            GivenLocalRequest();
+            bundles.Add(new HtmlTemplateBundle("~/test"));
+            PageDataScriptBundle bundle = null;
+            helper
+                .Setup(h => h.Reference(It.IsAny<PageDataScriptBundle>(), null))
+                .Callback<Bundle, string>((referenced, _) => bundle = (PageDataScriptBundle)referenced);
+
+            handler.ProcessRequest();
+
+            var script = bundle.Render();
+            script.ShouldContain("HtmlTemplates=[{\"Path\":\"~/test\",\"Url\":null,\"Assets\":[],\"References\":[],\"Size\":-1}];");
         }
 
         [Fact]
