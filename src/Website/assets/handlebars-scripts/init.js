30b4cc711b31bb3d4061c07f8125d5c475743b7f
David Durst <davidbdurst@gmail.com>
2012-08-23 12:37:04 -0400

    really ready for a pull

---
+++ src/Website/assets/handlebars-scripts/init.js
@@ -0,0 +1,31 @@
+﻿var i18n = {
+    translate: function () {
+        return 'Bonjour';
+    }
+};
+
+$(function(){
+    
+    var i18n = {
+        translate: function (s) {
+            var obj = {
+                'Name': 'Nom',
+                'The': 'Le'
+            };
+            return obj[s] || s;
+        }
+    };
+    
+    console.log(JST['test'].render({
+        author: {
+            first: 'Jean',
+            last: 'Luc'
+        },
+        i18n: function() {
+            return function (s) {
+                return (i18n && typeof i18n.translate === 'function') ? i18n.translate(s) : s;
+            };
+        }
+    }));
+
+});
