abd87d790346e47d5058b188e173d01fd1bb5a79
Andrew Davey <andrew@equin.co.uk>
2012-08-30 10:32:03 +0200

    Fix links to documentation when outside of documentation action

--- src/Website/Views/Helpers.cs
+++ src/Website/Views/Helpers.cs
@@ -4,6 +4,11 @@ namespace Website.Views
 {
     public static class Helpers
     {
+        public static string DocumentationUrl(this UrlHelper urlHelper, string path, string version = null)
+        {
+            return urlHelper.RouteUrl("Documentation", new { path, version });
+        }
+
         public static string DocumentationUrl(this UrlHelper urlHelper, string path)
         {
             return urlHelper.RouteUrl("Documentation", new { path });
