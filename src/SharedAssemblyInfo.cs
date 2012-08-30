5ad9ebd4b83e479c0150d4cfada5100742f170e4
Andrew Davey <andrew@equin.co.uk>
2012-08-30 10:57:21 +0200

    v2.0.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@
 [assembly: AssemblyProduct("Cassette")]
 [assembly: AssemblyCopyright("Copyright © 2011 Andrew Davey")]
 
-[assembly: AssemblyInformationalVersion("2.0.0-beta6")]
+[assembly: AssemblyInformationalVersion("2.0.0")]
 
 [assembly: AssemblyVersion("2.0.0.*")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
