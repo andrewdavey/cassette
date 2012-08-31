6db64435bea06718c132b5760f8ff0320e368ac5
Andrew Davey <andrew@equin.co.uk>
2012-08-31 18:25:45 +0200

    Disable file system watcher when cache manifest is static. #306

--- src/Cassette.Aspnet/WebHostSettingsConfiguration.cs
+++ src/Cassette.Aspnet/WebHostSettingsConfiguration.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Web;
 using System.Web.Configuration;
+using System.Xml.Linq;
 using Cassette.IO;
 using System.IO;
 
@@ -19,10 +21,12 @@ namespace Cassette.Aspnet
             var configurationSection = GetConfigurationSection();
             settings.IsDebuggingEnabled = configurationSection.Debug.HasValue ? configurationSection.Debug.Value : IsAspNetDebuggingEnabled;
             settings.IsHtmlRewritingEnabled = configurationSection.RewriteHtml;
-            settings.IsFileSystemWatchingEnabled = TrustLevel.IsFullTrust();
             settings.AllowRemoteDiagnostics = configurationSection.AllowRemoteDiagnostics;
             settings.SourceDirectory = new FileSystemDirectory(AppDomainAppPath);
             settings.CacheDirectory = GetCacheDirectory(configurationSection);
+            settings.IsFileSystemWatchingEnabled = TrustLevel.IsFullTrust() && !IsStaticCacheManifest(settings);
+
+            IsStaticCacheManifest(settings);
 
             // Include the virtual directory so that if the application is moved to 
             // another virtual directory the bundles will be rebuilt with the updated URLs.
@@ -44,6 +48,19 @@ namespace Cassette.Aspnet
             {
                 path = path.TrimStart('~', '/');
                 return new FileSystemDirectory(Path.Combine(AppDomainAppPath, path));
+            }
+        }
+
+        bool IsStaticCacheManifest(CassetteSettings settings)
+        {
+            var manifestFile = settings.CacheDirectory.GetFile("manifest.xml");
+            if (!manifestFile.Exists) return false;
+            using (var stream = manifestFile.OpenRead())
+            {
+                var doc = XDocument.Load(stream);
+                var attribute = doc.Root.Attribute("IsStatic");
+                return attribute != null
+                    && attribute.Value.Equals("true", StringComparison.OrdinalIgnoreCase);
             }
         }
 
