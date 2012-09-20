0332581090c77165adc7b98086d93d2b4c31e397
Andrew Davey <andrew@equin.co.uk>
2012-09-19 17:46:55 -0700

    Add JQueryTmplServices to auto-configure pipeline

---
+++ src/Cassette.JQueryTmpl/JQueryTmplServices.cs
@@ -0,0 +1,16 @@
+﻿using Cassette.BundleProcessing;
+using Cassette.TinyIoC;
+
+namespace Cassette.HtmlTemplates
+{
+    [ConfigurationOrder(20)]
+    public class JQueryTmplServices : IConfiguration<TinyIoCContainer>
+    {
+        public void Configure(TinyIoCContainer container)
+        {
+            container
+                .Register(typeof(IBundlePipeline<HtmlTemplateBundle>), typeof(JQueryTmplPipeline))
+                .AsMultiInstance();
+        }
+    }
+}
