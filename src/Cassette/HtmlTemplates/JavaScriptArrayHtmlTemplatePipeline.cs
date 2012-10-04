74c07525b982e6a87409e95cf8d66ca825b83a6e
prabirshrestha <mail@prabir.me>
2012-10-03 21:24:02 -0400

    JavaScriptArrayHtmlTemplatePipeline

---
+++ src/Cassette/HtmlTemplates/JavaScriptArrayHtmlTemplatePipeline.cs
@@ -0,0 +1,70 @@
+using System;
+using Cassette.BundleProcessing;
+using Cassette.Scripts;
+using Cassette.TinyIoC;
+
+namespace Cassette.HtmlTemplates
+{
+    public class JavaScriptArrayHtmlTemplatePipeline : BundlePipeline<HtmlTemplateBundle>
+    {
+        readonly TinyIoCContainer container;
+        readonly CassetteSettings settings;
+
+        public JavaScriptArrayHtmlTemplatePipeline(TinyIoCContainer container, CassetteSettings settings)
+            : base(container)
+        {
+            this.container = container;
+            this.settings = settings;
+            BuildPipeline();
+        }
+
+        void BuildPipeline()
+        {
+            TransformHtmlTemplatesIntoJavaScript();
+            Concatenate();
+            WrapJavaScriptHtmlTemplates();
+            MinifyIfNotDebugging();
+            AssignContentType();
+            AssignHash();
+            AssignRenderer();
+        }
+
+        void TransformHtmlTemplatesIntoJavaScript()
+        {
+            Add<AddHtmlTemplateToJavaScriptTransformers>();
+        }
+
+        void Concatenate()
+        {
+            Add(new ConcatenateAssets { Separator = Environment.NewLine });
+        }
+
+        void WrapJavaScriptHtmlTemplates()
+        {
+            Add(new WrapJavaScriptHtmlTemplates());
+        }
+
+        void MinifyIfNotDebugging()
+        {
+            if (!settings.IsDebuggingEnabled)
+            {
+                Add(new MinifyAssets(container.Resolve<IJavaScriptMinifier>()));
+            }
+        }
+
+        void AssignContentType()
+        {
+            Add(new AssignContentType("text/javascript"));
+        }
+
+        void AssignHash()
+        {
+            Add(new AssignHash());
+        }
+
+        void AssignRenderer()
+        {
+            Add(new AssignHtmlTemplateRenderer(container.Resolve<RemoteHtmlTemplateBundleRenderer>()));
+        }
+    }
+}
