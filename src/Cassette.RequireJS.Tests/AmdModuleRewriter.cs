033007d1c648f18c9e8e72cb386835556a20199a
Andrew Davey <andrew@equin.co.uk>
2012-10-20 11:40:20 +0100

    Add AmdModuleRewriter

---
+++ src/Cassette.RequireJS.Tests/AmdModuleRewriter.cs
@@ -0,0 +1,40 @@
+﻿using Should;
+using Xunit;
+
+namespace Cassette.RequireJS.Tests
+{
+    public class AmdModuleRewriterTests
+    {
+        [Fact]
+        public void ModulePathArgumentIsInsertedIntoDefineCall()
+        {
+            var rewriter = new AmdModuleRewriter();
+            var output = rewriter.InsertModulePathIntoDefineCall("define([],function(){})", "example");
+            output.ShouldEqual("define(\"example\",[],function(){})");
+        }
+
+        [Fact]
+        public void OnlyDefineFunctionCallIsRewritten()
+        {
+            var rewriter = new AmdModuleRewriter();
+            var output = rewriter.InsertModulePathIntoDefineCall("other([],function(){})", "example");
+            output.ShouldEqual("other([],function(){})");
+        }
+
+        [Fact]
+        public void NonTopLevelDefineCallIsRewritten()
+        {
+            var rewriter = new AmdModuleRewriter();
+            var output = rewriter.InsertModulePathIntoDefineCall("(function(){define([],function(){})})()", "example");
+            output.ShouldEqual("(function(){define(\"example\",[],function(){})})()");
+        }
+
+        [Fact]
+        public void DontInsertModulePathWhenItsAlreadyPresent()
+        {
+            var rewriter = new AmdModuleRewriter();
+            var output = rewriter.InsertModulePathIntoDefineCall("define(\"defined/path\",[],function(){})", "example");
+            output.ShouldEqual("define(\"defined/path\",[],function(){})");
+        }
+    }
+}
---
+++ src/Cassette.RequireJS/AmdModuleRewriter.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Ajax.Utilities;
+
+namespace Cassette.RequireJS
+{
+    public class AmdModuleRewriter
+    {
+        public string InsertModulePathIntoDefineCall(string moduleScript, string modulePath)
+        {
+            var parser = new JSParser(moduleScript);
+            var tree = parser.Parse(new CodeSettings());
+            tree.Accept(new Visitor(modulePath));
+
+            return tree.ToCode();
+        }
+
+        class Visitor : TreeVisitor
+        {
+            readonly string modulePath;
+
+            public Visitor(string modulePath)
+            {
+                this.modulePath = modulePath;
+            }
+
+            public override void Visit(CallNode node)
+            {
+                base.Visit(node);
+
+                if (ShouldInsertModulePathArgument(node))
+                {
+                    var modulePathNode = new ConstantWrapper(modulePath, PrimitiveType.String, node.Context, node.Parser);
+                    node.Arguments.Insert(0, modulePathNode);
+                }
+            }
+
+            bool ShouldInsertModulePathArgument(CallNode node)
+            {
+                return IsDefineFunction(node)
+                    && DefineCallIsAnonymous(node);
+            }
+
+            bool IsDefineFunction(CallNode node)
+            {
+                return node.Function.ToCode() == "define";
+            }
+
+            bool DefineCallIsAnonymous(CallNode node)
+            {
+                // e.g. define( [...], function() {} )
+                // or   define( function() {} )
+                return node.Arguments.Count < 3;
+            }
+        }
+    }
+}
