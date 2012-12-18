aebbeee9d2cb8d828a42972514e71a5656aecc86
Andrew Davey <andrew@equin.co.uk>
2012-12-18 16:43:54 +0000

    Remove KnockoutJS jQuery tmpl support
    jQuery tmpl is deprecated. The precompiler has also been broken
    since Knockout v2. The native template engine in Knockout is much
    nicer too. So it seems reasonable to remove this broken Cassette
    plugin.

--- src/Cassette.UnitTests/HtmlTemplates/JQueryTmplCompiler.cs
+++ src/Cassette.UnitTests/HtmlTemplates/JQueryTmplCompiler.cs
@@ -8,7 +8,7 @@ namespace Cassette.HtmlTemplates
         [Fact]
         public void WhenCompileHtmlWithSingleQuotes_ThenSingleQuotesAreEscaped()
         {
-            var compiler = new KnockoutJQueryTmplCompiler();
+            var compiler = new JQueryTmplCompiler();
             var result = compiler.Compile("<div id='a'></div>", new CompileContext());
             result.Output.ShouldEqual(
                 @"function(jQuery, $item) {var $=jQuery,call,__=[],$data=$item.data;with($data){__.push('<div id=\'a\'></div>');}return __;}"
