d81c363c32321a05b8ca10e499f5165b3471efde
Josh Sullivan <jaysche.sullivan@gmail.com>
2013-01-30 00:59:17 -0500

    Added Handlebars project
    Untested, but it all compiles...

---
+++ src/Cassette.Handlebars/CompileHandlebars.cs
@@ -0,0 +1,19 @@
+﻿using Cassette.BundleProcessing;
+
+namespace Cassette.HtmlTemplates
+{
+    public class CompileHandlebars : AddTransformerToAssets<HtmlTemplateBundle>
+    {
+        readonly CassetteSettings settings;
+
+        public CompileHandlebars(CassetteSettings settings)
+        {
+            this.settings = settings;
+        }
+
+        protected override IAssetTransformer CreateAssetTransformer(HtmlTemplateBundle bundle)
+        {
+            return new CompileAsset(new HandlebarsCompiler(), settings.SourceDirectory);
+        }
+    }
+}
---
+++ src/Cassette.Handlebars/HandlebarsCompiler.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Jurassic;
+
+namespace Cassette.HtmlTemplates
+{
+    public class HandlebarsCompiler : ICompiler
+    {
+        public HandlebarsCompiler()
+        {
+            scriptEngine = new ScriptEngine();
+            scriptEngine.Execute(Properties.Resources.handlebars);
+            
+            scriptEngine.Execute(@"var precompile = Handlebars.precompile;");
+        }
+
+
+        readonly ScriptEngine scriptEngine;
+
+        public CompileResult Compile(string source, CompileContext context)
+        {
+            //var javascript = scriptEngine.CallGlobalFunction<string>("precompile", source);
+            var handlebarsJs = string.Format("var {0} = Handlebars.template({1});", source, scriptEngine.CallGlobalFunction("precompile", source));
+            return new CompileResult(handlebarsJs, Enumerable.Empty<string>());
+        }
+    }
+}
