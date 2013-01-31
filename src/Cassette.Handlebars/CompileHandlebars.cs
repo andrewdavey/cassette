8d43ab2fb1fbcaa5b335c0fc49bf311367db8956
Josh Sullivan <jaysche.sullivan@gmail.com>
2013-01-30 23:29:44 -0500

    Support for KnownHelpers setting compilation

--- src/Cassette.Handlebars/CompileHandlebars.cs
+++ src/Cassette.Handlebars/CompileHandlebars.cs
@@ -5,15 +5,22 @@ namespace Cassette.HtmlTemplates
     public class CompileHandlebars : AddTransformerToAssets<HtmlTemplateBundle>
     {
         readonly CassetteSettings settings;
+        readonly HandlebarsSettings handlebarsSettings;
 
         public CompileHandlebars(CassetteSettings settings)
         {
             this.settings = settings;
         }
 
+        public CompileHandlebars(CassetteSettings settings, HandlebarsSettings handlebarsSettings)
+        {
+            this.settings = settings;
+            this.handlebarsSettings = handlebarsSettings;
+        }
+
         protected override IAssetTransformer CreateAssetTransformer(HtmlTemplateBundle bundle)
         {
-            return new CompileAsset(new HandlebarsCompiler(), settings.SourceDirectory);
+            return new CompileAsset(new HandlebarsCompiler(handlebarsSettings), settings.SourceDirectory);
         }
     }
 }
--- src/Cassette.Handlebars/HandlebarsCompiler.cs
+++ src/Cassette.Handlebars/HandlebarsCompiler.cs
@@ -5,16 +5,21 @@ namespace Cassette.HtmlTemplates
 {
     public class HandlebarsCompiler : ICompiler
     {
-        public HandlebarsCompiler()
+        public HandlebarsCompiler(HandlebarsSettings settings)
         {
             scriptEngine = new ScriptEngine();
             scriptEngine.Execute(Properties.Resources.handlebars);
 
+            var knownHelpersFormattedList = settings.KnownHelpers.Aggregate("[", (current, helper) => current + ("'" + helper + "'"));
+            knownHelpersFormattedList += "]";
+
+
             // Create a global compile function that returns the template compiled into javascript source.
             scriptEngine.Execute(@"
                 var precompile = function (template) {
-                  return Handlebars.precompile(template, { knownHelpers : ['t', 'eachkeys'], knownHelpersOnly: false });
-                };");
+                    return Handlebars.precompile(template, { knownHelpers: "+ knownHelpersFormattedList +@", knownHelpersOnly: false });
+                };"
+            );
         }
         
         readonly ScriptEngine scriptEngine;
