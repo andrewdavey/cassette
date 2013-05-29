65c198609ba9b162c97709c2de84eab8a417ec46
Daniel Lo Nigro <daniel@dan.cx>
2013-05-29 22:00:23 +1000

    BundleCollection helper method for adding embedded resource bundles

--- src/Cassette.Aspnet/DiagnosticBundleConfiguration.cs
+++ src/Cassette.Aspnet/DiagnosticBundleConfiguration.cs
@@ -1,30 +1,21 @@
-﻿using System.Linq;
-using System.Reflection;
-using Cassette.IO;
+﻿using System.Reflection;
 using Cassette.Scripts;
 
 namespace Cassette.Aspnet
 {
     public class DiagnosticBundleConfiguration : IConfiguration<BundleCollection>
     {
-        readonly IBundleFactory<ScriptBundle> scriptBundleFactory;
-
-        public DiagnosticBundleConfiguration(IBundleFactory<ScriptBundle> scriptBundleFactory)
-        {
-            this.scriptBundleFactory = scriptBundleFactory;
-        }
-
         public void Configure(BundleCollection bundles)
         {
-            var scriptBundle = scriptBundleFactory.CreateBundle(
-                "~/Cassette.Aspnet.Resources",
-                Enumerable.Empty<IFile>(),
-                new BundleDescriptor { AssetFilenames = { "*" } }
-                );
-            scriptBundle.Assets.Add(new ResourceAsset("Cassette.Aspnet.Resources.jquery.js", Assembly.GetExecutingAssembly()));
-            scriptBundle.Assets.Add(new ResourceAsset("Cassette.Aspnet.Resources.knockout.js", Assembly.GetExecutingAssembly()));
-            scriptBundle.Assets.Add(new ResourceAsset("Cassette.Aspnet.Resources.diagnostic-page.js", Assembly.GetExecutingAssembly()));
-            bundles.Add(scriptBundle);
+			bundles.AddEmbeddedResources<ScriptBundle>(
+				"~/Cassette.Aspnet.Resources",
+				Assembly.GetExecutingAssembly(),
+				"Cassette.Aspnet.Resources",
+
+				"jquery.js",
+				"knockout.js",
+				"diagnostic-page.js"
+			);
         }
     }
 }
